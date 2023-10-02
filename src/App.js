d35695eed3913609800071a846a785e3d5839a8f
Tamer <tamerchoyy@gmail.com>
2023-10-02 23:45:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Navigation from './components/Navigation/Navigation';
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+
 function App() {
-  return (
-    <div className="py-10">
-        TEST
-    </div>
-  );
+    return (
+    <>
+        <Navigation/>
+
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+        </Routes>
+
+        <Footer/>
+    </>
+    );
 }
 
 export default App;
