f8cf9908aee926a4081e4cff001744c6078e7555
Tamer <tamerchoyy@gmail.com>
2023-10-07 21:41:24 +0300

    added logging in function

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import AuthContext from '../../context/AuthContext';
 
 const Login = () => {
     const [passwordEye, setPasswordEye] = useState(false);
+    const { loginUser } = useContext(AuthContext);
 
     return (
         <article className="flex justify-center items-center p-16">
-            <form className="flex flex-col basis-1/4 gap-8 p-12 rounded-xl text-xl bg-white">
+            <form onSubmit={loginUser} className="flex flex-col basis-1/4 gap-8 p-12 rounded-xl text-xl bg-white">
                 <section className="flex flex-col gap-4">
                     <label className="font-medium" htmlFor="username">Потребителско име</label>
-                    <input className="p-2 border-solid border-2 border-black rounded-xl" type="text" id="username"></input>
+                    <input 
+                        className="p-2 border-solid border-2 border-black rounded-xl" 
+                        type="text" 
+                        id="username" 
+                        name="username">
+                    </input>
                 </section>
 
                 <section className="flex flex-col gap-4">
@@ -17,7 +24,8 @@ const Login = () => {
                         <input 
                             className="p-2 border-solid border-2 border-black rounded-xl w-full" 
                             type={passwordEye ? "text": "password"}
-                            id="password">
+                            id="password"
+                            name="password">
                         </input>
                         <i
                             onClick={() => setPasswordEye(!passwordEye)}
@@ -27,6 +35,7 @@ const Login = () => {
                 </section>
 
                 <button 
+                    type="submit"
                     className="bg-[#10ACDB] text-white rounded-xl p-4 hover:bg-cyan-800">
                     Влез
                 </button>
