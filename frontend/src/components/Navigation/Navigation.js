5278de56fbc66e0d78a5b159c94f76f6e10a5e55
Tamer <tamerchoyy@gmail.com>
2023-10-07 21:42:08 +0300

    added ternary for logged and not logged in users

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import AuthContext from '../../context/AuthContext';
 
 const Navigation = () => {
+    const {user, logoutUser} = useContext(AuthContext)
+
     return (
         <header className="flex justify-center p-6 bg-gradient-to-r from-blue-500 to-cyan-500 text-white font-medium text-2xl">
             <nav className="flex justify-between w-5/6">
@@ -15,12 +19,24 @@ const Navigation = () => {
                     <li className="hover:text-stone-300 duration-300">
                         <Link to="/burgas">Бургас</Link>
                     </li>
-                    <li className="hover:text-stone-300 duration-300">
-                        <Link to="/login">Влез</Link>
-                    </li>
-                    <li className="hover:text-stone-300 duration-300">
-                        <Link to="/register">Регистрация</Link>
-                    </li>
+
+                    {user ? (
+                        <li 
+                            onClick={logoutUser}
+                            className="hover:text-stone-300 duration-300 hover:cursor-pointer">
+                            Излез
+                        </li>
+                    ) : (
+                        <>
+                            <li className="hover:text-stone-300 duration-300">
+                                <Link to="/login">Влез</Link>
+                            </li>
+                            <li className="hover:text-stone-300 duration-300">
+                                <Link to="/register">Регистрация</Link>
+                            </li>
+                        </>
+                    )}
+
                 </ul>
             </nav>
         </header>
