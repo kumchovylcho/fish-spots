e999de9c8205e3ad3a935983fa1fe8a55f7ed456
Tamer <tamerchoyy@gmail.com>
2023-10-07 03:46:52 +0300

    update

--- backend/base/api/serializers.py
+++ backend/base/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -15,30 +17,13 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    rePassword = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password', 'rePassword')
-
-    def validate(self, data):
-        errors = {}
-
-        existing_username = User.objects.filter(username=data['username']).first()
-        if existing_username:
-            errors['username'] = "A user with this username already exists."
-
-        existing_email = User.objects.filter(email=data['email']).first()
-        if existing_email:
-            errors['email'] = "A user with this email already exists."
-
-        if errors:
-            raise serializers.ValidationError(errors)
-
-        return data
+        model = UserModel
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = UserModel.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
