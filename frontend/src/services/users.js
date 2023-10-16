3436930be27784b3dde86b84afd66dfb3d8b6119
Tamer <tamerchoyy@gmail.com>
2023-10-16 12:32:22 +0300

    added login, logout

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,3 +1,6 @@
+import jwt_decode from 'jwt-decode';
+
+
 const baseUrl = "http://127.0.0.1:8000/api/"
 
 
@@ -12,4 +15,54 @@ const register = async (data) => {
 }
 
 
-export {register};
+const loginUser = async (e, setAuthTokens, setUser, navigate, navigateTo, setError="") => {
+    e.preventDefault();
+
+    const userCredentials = {
+        "username": e.target.username.value,
+        "password": e.target.password.value
+    }
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userCredentials)
+    };
+
+    const response = await fetch(`${baseUrl}token/`, options);
+    const data = await response.json();
+
+    if (response.status === 200) {
+        setAuthTokens(data);
+        setUser(jwt_decode(data.access));
+        localStorage.setItem("authTokens", JSON.stringify(data));
+        navigate(navigateTo);
+    }else {
+        if (setError) {
+            setError(true);
+        } 
+    }
+}
+
+
+const logoutUser = async (authTokens, setAuthTokens, setUser, navigate, navigateTo) => {
+    const response = await fetch(`${baseUrl}logout/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({refreshToken: authTokens?.refresh}),
+    });
+
+    if (response.status === 200) {
+        setAuthTokens("");
+        setUser("");
+        localStorage.removeItem("authTokens");
+        navigate(navigateTo);
+    }
+}
+
+
+export {register, loginUser, logoutUser, baseUrl};
