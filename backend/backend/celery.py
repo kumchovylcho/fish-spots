dea0a73d55ae1573fbf8b9ba724f2b89bb9288cc
Tamer <tamerchoyy@gmail.com>
2023-10-17 22:36:58 +0300

    update

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,8 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from datetime import timedelta
-from backend import settings
+from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
