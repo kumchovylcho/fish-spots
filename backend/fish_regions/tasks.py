bbb23b3fa7c3ee1dfea836022430bfd1fadff3ce
Tamer <tamerchoyy@gmail.com>
2023-10-19 22:33:49 +0300

    addd execute logic

--- backend/fish_regions/tasks.py
+++ backend/fish_regions/tasks.py
@@ -1,24 +1,58 @@
 from backend.celery import app
-from fish_regions.models import VarnaRegion
+from fish_regions.models import VarnaRegion, BurgasRegion
+from . import helpers
+import requests
+
 
 
 @app.task()
-def update_regions():
-    region = VarnaRegion(city_name="asd",
-                         sunrise="asd",
-                         sunset="asd",
-                         date="asd",
-                         time="asd",
-                         feels_like=1,
-                         normal_temperature=1,
-                         min_temperature=1,
-                         max_temperature=1,
-                         weather_icon_url="asd",
-                         wind_direction="asd",
-                         wind_speed=1
-                        )
-    
-    region.save()
+def update_regions() -> None:
+    regions = {
+        "varna": {"places": (
+                            (43.5163252, 28.5932185),
+                            (43.322895, 28.0594381),
+                            (43.1997359, 27.9140717)
+                            ),
+                "list_place_names": ["Shabla", "Kranevo", "Varna"],
+                "operation": "",
+                "model": VarnaRegion,
+        },
+
+        "burgas": {"places": (
+                            (42.4857915, 27.4752997),
+                            (42.4575539, 27.6262701),
+                            (42.2665013, 27.7619339)
+                            ),
+                "list_place_names": ["Burgas", "Chernomorets", "Primorsko"],
+                "operation": "",
+                "model": BurgasRegion,
+        },
+    }
+
+    operation_caller = {
+        "update": helpers.update_region_model,
+        "create": helpers.create_region_object,
+    }
+
+    for data in regions.values():
+        if helpers.place_in_region_exist(data["model"], data["list_place_names"]):
+            data["operation"] = "update"
+        else:
+            data["operation"] = "create"
+
+        for longitude, latitude in data["places"]:
+            query = helpers.get_query(longitude, latitude)
+            try:
+                response = requests.get(query)
+            except:
+                continue
+
+            if response.status_code != 200:
+                continue
+
+            weather_data = response.json()
+
+            operation_caller[data["operation"]](data["model"], weather_data)
 
 
 update_regions.delay()
