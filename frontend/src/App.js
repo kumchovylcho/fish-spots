2e72d9c0038f505ce3f1349de27fbe93f84d7263
Tamer <tamerchoyy@gmail.com>
2023-10-21 20:35:28 +0300

    protected register and added new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import { AuthProvider } from './context/AuthContext';
 import Navigation from './components/Navigation/Navigation';
@@ -8,31 +7,34 @@ import Footer from './components/Footer/Footer';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import PrivateRoute from './util/PrivateRoute';
+import Varna from './components/Varna/Varna';
+import Burgas from './components/Burgas/Burgas';
 
 
 function App() {
 
-    useEffect(() => {
-        document.body.style.backgroundColor = '#D4D4D8';
-
-      }, []);
-
     return (
     <>
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-zinc-300">
         <AuthProvider>
             <Navigation/>
 
             <Routes>
             
                 <Route path="/" element={<Home/>}/>
+                <Route path="/varna" element={<Varna/>}/>
+                <Route path="/burgas" element={<Burgas/>}/>
                 <Route path="/login" element={
                                             <PrivateRoute navigateTo="/">
                                                 <Login/>
                                             </PrivateRoute>
                                             }
                                             />
-                <Route path="/register" element={<Register/>}/>
+                <Route path="/register" element={
+                                                <PrivateRoute navigateTo="/">
+                                                    <Register/>
+                                                </PrivateRoute>
+                                                }/>
             </Routes>
         </AuthProvider>
 
