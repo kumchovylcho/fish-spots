37e47fedb34d94e2be2529cf8704c6b5db642d3f
Tamer <tamerchoyy@gmail.com>
2023-10-21 20:39:20 +0300

    update fetch urls

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -16,7 +16,7 @@ export const AuthProvider = ({children}) => {
     const navigate = useNavigate();
 
     const updateToken = async () => {
-        const response = await fetch(`${baseUrl}refresh/`, {
+        const response = await fetch(`${baseUrl}/api/refresh/`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
