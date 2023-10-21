ff6c18ddf5a004a8e024c4254afdc676f9e164a3
Tamer <tamerchoyy@gmail.com>
2023-10-21 20:38:29 +0300

    added region card

---
+++ frontend/src/components/RegionChoiceCard/RegionChoiceCard.js
@@ -0,0 +1,42 @@
+export default function RegionChoiceCard({
+    imgUrl,
+    imgAlt,
+    pText,
+    dataKey,
+    showData,
+    setShowData,
+}) {
+
+    const isSelectedClasses = () => {
+        return showData[dataKey]
+            ? 'scale-110 bg-zinc-300'
+            : 'hover:scale-105 bg-white duration-150';
+    };
+
+    const selectCard = () => {
+        const resetData = {}
+        for (const key of Object.keys(showData)) {
+            resetData[key] = false;
+        }
+
+        resetData[dataKey] = true;
+        setShowData(resetData);
+    }
+
+
+    return (
+        <section
+            className={`basis-[26%] p-2 rounded-xl cursor-pointer ${isSelectedClasses()}`}
+            onClick={selectCard}
+            >
+            <div className="mb-3">
+                <img
+                    src={imgUrl}
+                    className="w-9/12 h-auto mx-auto"
+                    alt={imgAlt}
+                />
+            </div>
+            <p>{pText}</p>
+        </section>
+    );
+}
