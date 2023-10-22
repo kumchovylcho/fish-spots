3b5febe8c7bfab07864d9b549a4db56d4323ffb9
Tamer <tamerchoyy@gmail.com>
2023-10-23 01:31:38 +0300

    replaced width with max-width

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -9,7 +9,7 @@ const Navigation = () => {
 
     return (
         <header className="flex justify-center p-6 bg-gradient-to-r from-blue-500 to-cyan-500 text-white font-medium text-2xl">
-            <nav className="flex justify-between w-5/6">
+            <nav className="flex justify-between w-5/6 max-w-screen-2xl">
                 <h1 className="hover:text-stone-300 duration-300">
                     <Link to="/">Риболовни места</Link>
                 </h1>
