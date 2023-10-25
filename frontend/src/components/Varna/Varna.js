7e95e65bdff802f51b50beecd1f64b7a9b8b574e
Tamer <tamerchoyy@gmail.com>
2023-10-25 23:33:11 +0300

    now fetching the cached results when choosing a city

--- frontend/src/components/Varna/Varna.js
+++ frontend/src/components/Varna/Varna.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
-import { getWeather } from '../../services/weather';
 import RegionChoiceCard from '../RegionChoiceCard/RegionChoiceCard';
 import TodayWeatherCard from '../TodayWeatherCard/TodayWeatherCard';
+import SelectCity from '../SelectCity/SelectCity';
+import { getWeather } from '../../services/weather';
+
 
 export default function Varna() {
     const [showData, setShowData] = useState({
@@ -10,15 +12,18 @@ export default function Varna() {
         suggestedSpots: false,
     });
 
+    const [cityKey, setCityKey] = useState("");
     const [weatherData, setWeatherData] = useState({});
 
     useEffect(() => {
-        getWeather('north', 'varna').then((data) => {
+        getWeather().then((data) => {
             setWeatherData((prevData) => {
                 return { ...prevData, ...data };
             });
         });
-    }, []);
+
+    }, [cityKey]);
+
 
     function renderTodayWeatherCard(data, showSunset = false) {
         return (
@@ -56,7 +61,7 @@ export default function Varna() {
             <section className="text-center py-16 text-5xl">
                 <h2>Регион Варна</h2>
             </section>
-            <div className="flex flex-wrap justify-around max-w-7xl mx-auto text-2xl text-center bg-slate-500 text-black p-10 rounded-xl font-medium">
+            <div className="flex flex-wrap justify-around max-w-7xl mx-auto text-2xl text-center bg-slate-500 text-black p-10 mb-20 rounded-xl font-medium">
                 <RegionChoiceCard
                     imgUrl="https://cdn.discordapp.com/attachments/1156335620919152650/1165310078774165575/weather.png?ex=65466295&is=6533ed95&hm=a87916564083109acf2ca9cd386299b8e8298147b255054b676fc8455f45473b&"
                     imgAlt="прогноза"
@@ -83,29 +88,45 @@ export default function Varna() {
                 />
             </div>
 
-            {Object.keys(weatherData).length && showData.weather ? (
+            {showData.weather && Object.keys(weatherData).length ? (
                 <>
-                    <h3 className="max-w-7xl mx-auto text-center py-16 text-3xl relative">
-                        Прогноза за днес - Варна
-                        <div className="absolute top-[65%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-40 h-0.5 bg-cyan-600"></div>
-                    </h3>
-                    <article className="max-w-screen-2xl mx-auto bg-cyan-700 py-7 rounded-xl mb-32">
-                        {renderTodayWeatherCard(
-                            weatherData.today, true
-                        )}
-                        {renderTodayWeatherCard(
-                            weatherData.four_days.first_day
-                        )}
-                        {renderTodayWeatherCard(
-                            weatherData.four_days.second_day
-                        )}
-                        {renderTodayWeatherCard(
-                            weatherData.four_days.third_day
-                        )}
-                        {renderTodayWeatherCard(
-                            weatherData.four_days.fourth_day
-                        )}
-                    </article>
+                    <SelectCity city1={"Шабла"} city2={"Кранево"} city3={"Варна"} setCityKey={setCityKey}/>
+                    
+                    { cityKey ? (
+                        <>
+                            <article className="max-w-screen-2xl mx-auto bg-cyan-700 rounded-xl mb-32">
+                                <h3 className="max-w-7xl mx-auto text-center py-10 text-3xl relative">
+                                    Прогноза за днес - {weatherData.varna[cityKey].today.bg_name_place}
+                                    <div className="absolute top-[75%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-60 h-0.5 bg-white"></div>
+                                </h3>
+                                {renderTodayWeatherCard(
+                                    weatherData.varna[cityKey].today, true
+                                )}
+                            </article>
+                            
+                            <article className="max-w-screen-2xl mx-auto bg-cyan-700 py-7 rounded-xl mb-32">
+                                <h3 className="max-w-7xl mx-auto text-center py-10 text-3xl relative">
+                                    4 дневна прогноза
+                                    <div className="absolute top-[75%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-40 h-0.5 bg-white"></div>
+                                </h3>
+                                {renderTodayWeatherCard(
+                                    weatherData.varna[cityKey].four_days.first_day
+                                )}
+                                {renderTodayWeatherCard(
+                                    weatherData.varna[cityKey].four_days.second_day
+                                )}
+                                {renderTodayWeatherCard(
+                                    weatherData.varna[cityKey].four_days.third_day
+                                )}
+                                {renderTodayWeatherCard(
+                                    weatherData.varna[cityKey].four_days.fourth_day
+                                )}
+                            </article>
+                        </>
+                    ) : (
+                        ""
+                    )
+                    }
                 </>
             ) : (
                 ''
