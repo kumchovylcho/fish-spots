933800aed6f27167ffae1e22adfbc0372bd5b00d
Tamer <tamerchoyy@gmail.com>
2023-10-26 23:51:30 +0300

    created place model

---
+++ backend/fish_places/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Place(models.Model):
+
+    place = models.CharField(max_length=50)
+
+    description = models.TextField()
+
+    image_url = models.TextField()
+
+    longitude = models.CharField(max_length=30)
+
+    latitude = models.CharField(max_length=30)
+
+    region = models.CharField(max_length=30)
