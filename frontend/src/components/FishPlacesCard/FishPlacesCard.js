e32e2bc0b59f7ce4dcb204eade0737a56407c6a4
Tamer <tamerchoyy@gmail.com>
2023-10-29 01:44:03 +0300

    fixed card width, added lazy loading and shadows

--- frontend/src/components/FishPlacesCard/FishPlacesCard.js
+++ frontend/src/components/FishPlacesCard/FishPlacesCard.js
@@ -1,7 +1,7 @@
 export default function FishPlacesCard({ props, modalOpen }) {
 
     return (
-        <section className="max-w-[200px] text-center rounded p-3 bg-cyan-950 shadow-2xl shadow-black">
+        <section className="max-w-[224px] text-center rounded p-3 bg-cyan-950 shadow-2xl shadow-black">
             <h3 className="font-medium mb-2 text-white">
                 {props.bg_place_name}
             </h3>
@@ -13,13 +13,13 @@ export default function FishPlacesCard({ props, modalOpen }) {
                     loading="lazy"
                 />
             </div>
-            <section className="bg-white rounded-b py-2">
+            <section className="bg-white rounded-b py-2 max-h-[200px] px-1">
                 <p className="mb-3">
                     {props.description.slice(0, 50)}...
                 </p>
                 <button 
                     className="text-center bg-cyan-600 py-2 px-4 rounded-xl font-medium hover:bg-cyan-800"
-                    onClick={() => modalOpen()}
+                    onClick={() => modalOpen(props.id)}
                     >
                     Виж повече
                 </button>
