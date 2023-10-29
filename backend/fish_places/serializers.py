45faa9eacb46b8d4fecf59325694a78ba429731a
Tamer <tamerchoyy@gmail.com>
2023-10-30 00:51:12 +0200

    added string into list method

--- backend/fish_places/serializers.py
+++ backend/fish_places/serializers.py
@@ -3,6 +3,12 @@ from fish_places.models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+
+    bad_wind_directions = serializers.SerializerMethodField()
+
     class Meta:
         model = Place
         fields = "__all__"
+
+    def get_bad_wind_directions(self, obj):
+        return obj.bad_wind_directions.split(", ")
