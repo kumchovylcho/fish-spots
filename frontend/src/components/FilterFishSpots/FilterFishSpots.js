4f606845b99259c170ed7d4b8566962e2e5b50e7
Tamer <tamerchoyy@gmail.com>
2023-10-31 23:36:58 +0200

    filter fish spots component

---
+++ frontend/src/components/FilterFishSpots/FilterFishSpots.js
@@ -0,0 +1,13 @@
+export default function FilterFishSpots ({ filterFishSpots }) {
+
+    return (
+        <section className="max-w-lg mx-auto mb-6">
+            <input 
+                className="w-full p-2 rounded-lg outline-none"
+                type="text"
+                placeholder="Потърси място..."
+                onChange={(e) => {filterFishSpots(e.target.value)}}
+            />
+        </section>
+    );
+}
