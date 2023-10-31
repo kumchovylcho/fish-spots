1db523adc935dc93c435fb6def8dd4aa3ba5fb7e
Tamer <tamerchoyy@gmail.com>
2023-10-31 23:37:17 +0200

    hiding a tag if logged in

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom'
+import AuthContext from '../../context/AuthContext';
+import setDocTitle from '../../util/setDocTitle';
+import { useContext } from 'react';
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
+
+    setDocTitle("Home");
+
     return (
     <main>
         <div className="flex justify-center items-center h-96 min-h-fit overflow-hidden relative">
@@ -17,7 +24,7 @@ const Home = () => {
 
                 <Link
                     to="/register"
-                    className="bg-emerald-400 self-center px-10 py-4 text-2xl rounded-full font-medium hover:bg-teal-600 duration-300 hover:scale-105">
+                    className={`${user ? "hidden": "block"} bg-emerald-400 self-center px-10 py-4 text-2xl rounded-full font-medium hover:bg-teal-600 duration-300 hover:scale-105`}>
                     Регистрирай се!
                 </Link>
 
@@ -30,9 +37,9 @@ const Home = () => {
                 <div className="absolute top-[115%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-60 h-0.5 bg-cyan-600"></div>
             </h2>
 
-            <div className="flex justify-center gap-x-12 w-3/5 mr-auto ml-auto">
+            <div className="w-full flex flex-wrap justify-center gap-12 w-3/5">
                 <Link to="/varna">
-                    <section className="bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
+                    <section className="max-w-lg bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
                         <h3 className="text-2xl text-center font-bold mb-6">ВАРНА</h3>
                         <div className="flex justify-center gap-x-12">
                             <section className="text-2xl">
@@ -60,7 +67,7 @@ const Home = () => {
                 </Link>
 
                 <Link to="burgas/">
-                    <section className="bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
+                    <section className="max-w-lg bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
                         <h3 className="text-2xl text-center font-bold mb-6">БУРГАС</h3>
                         <div className="flex justify-center gap-x-12">
                             <section className="text-2xl">
