9d2d6c70417fd1e2ad6d78b267da72d48a0a5455
Tamer <tamerchoyy@gmail.com>
2023-11-03 19:26:11 +0200

    added new path

--- backend/fish_regions/urls.py
+++ backend/fish_regions/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import (WeatherDataView, )
+from .views import (WeatherDataView,
+                    WeatherPlaceView
+                    )
 
 urlpatterns = [
     path('weather/', WeatherDataView.as_view(), name='weather_data'),
+    path('weather/<place>/', WeatherPlaceView.as_view(), name='weather_for_place'),
 ]
