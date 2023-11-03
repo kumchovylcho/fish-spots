391b96ec251ff5d7c4e879900af05558eb410930
Tamer <tamerchoyy@gmail.com>
2023-11-03 19:26:31 +0200

    added new view and modified the cache duration

--- backend/fish_regions/views.py
+++ backend/fish_regions/views.py
@@ -6,17 +6,11 @@ from . import helpers
 from django.core.cache import cache
 
 
-cache_key = "weather_cache"
-
-
 class WeatherDataView(APIView):
     http_method_names = ["get"]
 
     def get(self, request, *args, **kwargs):
-        cached_weather = cache.get(cache_key)
-
-        if cached_weather:
-            return Response(cached_weather, status=status.HTTP_200_OK)
+        region = request.GET.get("region", "").lower()
 
         regions = {
             "varna": {"model": VarnaRegion,
@@ -27,21 +21,51 @@ class WeatherDataView(APIView):
                       }
         }
 
+        if not regions.get(region):
+            return Response({"message": "Valid regions are varna/burgas."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        cached_weather = cache.get(region)
+        if cached_weather:
+            return Response(cached_weather, status=status.HTTP_200_OK)
+
         regions_data = {}
+        for place in regions[region]["places"]:
+            data_24h = helpers.get_24h_data(regions[region]["model"], place)
+            four_days_data = helpers.get_four_days_data(regions[region]["model"], place)
 
-        for region, data in regions.items():
-            model = data["model"]
+            regions_data[place.lower()] = {
+                "today": data_24h,
+                "four_days": four_days_data
+            }
 
-            regions_data[region] = {}
-            for place in data["places"]:
-                data_24h = helpers.get_24h_data(model, place)
-                four_days_data = helpers.get_four_days_data(model, place)
-
-                regions_data[region][place.lower()] = {
-                    "today": data_24h,
-                    "four_days": four_days_data
-                }
-
-        cache.set(cache_key, regions_data, timeout=1 * 60 * 60 * 3)  # 3 hours
+        cache.set(region, regions_data, timeout=1 * 60 * 20)  # 20 minutes
 
         return Response(regions_data, status=status.HTTP_200_OK)
+
+
+class WeatherPlaceView(APIView):
+    http_method_names = ["get"]
+
+    def get(self, request, place, *args, **kwargs):
+        region = request.GET.get("region", "").lower()
+        place = place.lower()
+
+        if not region or region not in ["varna", "burgas"]:
+            return Response({"message": "You must pass a valid region. Example -> varna/burgas"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        valid_places = ("shabla", "kranevo", "varna", "burgas", "chernomorets", "primorsko")
+        if place not in valid_places:
+            return Response({"message": f"You must pass a valid place. Example -> {valid_places}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        model = VarnaRegion if region == "varna" else BurgasRegion
+
+        place = place.capitalize()
+        data_24h = helpers.get_24h_data(model, place)
+        four_days_data = helpers.get_four_days_data(model, place)
+
+        place_data = {
+            "today": data_24h,
+            "four_days": four_days_data
+        }
+
+        return Response(place_data, status=status.HTTP_200_OK)
