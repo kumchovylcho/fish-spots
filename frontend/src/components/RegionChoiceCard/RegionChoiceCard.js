29c31fdc44693eeeee29a988511e2fef74884f8a
Tamer <tamerchoyy@gmail.com>
2023-11-03 19:39:02 +0200

    fixed stylings

--- frontend/src/components/RegionChoiceCard/RegionChoiceCard.js
+++ frontend/src/components/RegionChoiceCard/RegionChoiceCard.js
@@ -1,44 +1,29 @@
+import { Link } from 'react-router-dom';
+
+
 export default function RegionChoiceCard({
     imgUrl,
     imgAlt,
     pText,
-    dataKey,
-    showData,
-    setShowData,
-    resetCityKey
+    linkTo,
+    shouldScale
 }) {
 
-    const isSelectedClasses = () => {
-        return showData[dataKey]
-            ? 'scale-105 bg-zinc-300'
-            : 'hover:scale-105 hover:bg-zinc-300 bg-white duration-150';
-    };
-
-    const selectCard = () => {
-        const resetData = {}
-        for (const key of Object.keys(showData)) {
-            resetData[key] = false;
-        }
-
-        resetData[dataKey] = true;
-        setShowData(resetData);
-        resetCityKey();
-    }
-
-
     return (
-        <section
-            className={`basis-[26%] p-2 rounded-xl cursor-pointer ${isSelectedClasses()}`}
-            onClick={selectCard}
-            >
-            <div className="mb-3">
-                <img
-                    src={imgUrl}
-                    className="w-9/12 h-auto mx-auto"
-                    alt={imgAlt}
-                />
-            </div>
-            <p>{pText}</p>
-        </section>
+        <Link 
+            to={linkTo}
+            className={`basis-[26%] p-2 rounded-xl cursor-pointer hover:scale-105 hover:bg-zinc-300 bg-white duration-150 ${shouldScale && "scale-105 bg-zinc-300"}`}
+        >
+            <section>
+                <div className="mb-3">
+                    <img
+                        src={imgUrl}
+                        className="w-9/12 h-auto mx-auto"
+                        alt={imgAlt}
+                    />
+                </div>
+                <p>{pText}</p>
+            </section>
+        </Link>
     );
 }
