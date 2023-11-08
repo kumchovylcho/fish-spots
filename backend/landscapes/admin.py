e43325b30686d1ea9dbac90df994811a7aec0fd7
Tamer <tamerchoyy@gmail.com>
2023-11-08 12:02:27 +0200

    included in django/admin

--- backend/landscapes/admin.py
+++ backend/landscapes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Landscape
+
+@admin.register(Landscape)
+class LandscapeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'created_at', 'author']
