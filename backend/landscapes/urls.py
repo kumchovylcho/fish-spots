cd67ed55d2ad47acb99a9875500964a70ede39d0
Tamer <tamerchoyy@gmail.com>
2023-11-08 12:03:30 +0200

    added list path

--- backend/landscapes/urls.py
+++ backend/landscapes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateLandscape
+from .views import CreateLandscape, ListLandscapes
 
 urlpatterns = (
     path('create', CreateLandscape.as_view(), name='create_landscape'),
+    path('list', ListLandscapes.as_view(), name='list_landscapes'),
 )
