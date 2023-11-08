bbe5715157a8a55283440def976ebed5953116b4
Tamer <tamerchoyy@gmail.com>
2023-11-08 12:01:56 +0200

    added pagination settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 9
 }
 
 SIMPLE_JWT = {
