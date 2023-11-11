52964c6ce00bf8f9707ed925af55b20e268e48e7
Tamer <tamerchoyy@gmail.com>
2023-11-11 21:09:29 +0200

    updated discord images to ibb

--- frontend/src/components/ChoiceContainer/ChoiceContainer.js
+++ frontend/src/components/ChoiceContainer/ChoiceContainer.js
@@ -13,21 +13,21 @@ export default function ChoiceContainer({
     return (
         <div className="flex flex-wrap justify-around max-w-7xl mx-auto text-2xl text-center bg-slate-500 text-black p-10 mb-20 rounded-xl font-medium">
             <RegionChoiceCard
-                imgUrl="https://cdn.discordapp.com/attachments/1156335620919152650/1165310078774165575/weather.png?ex=65466295&is=6533ed95&hm=a87916564083109acf2ca9cd386299b8e8298147b255054b676fc8455f45473b&"
+                imgUrl="https://i.ibb.co/bK3jFs1/weather-icon.png"
                 imgAlt="прогноза"
                 pText="Прогноза за времето"
                 linkTo={linkWeather}
                 shouldScale={pathName.includes("weather")}
             />
             <RegionChoiceCard
-                imgUrl="https://cdn.discordapp.com/attachments/1156335620919152650/1165310390880714934/fish-rod.png?ex=654662df&is=6533eddf&hm=76ef89e86ef21eedecb16ae7637ae23cd5cddbe93c04eeec45f1dad6ce32ae38&"
+                imgUrl="https://i.ibb.co/DtGhM0x/fish-rod.png"
                 imgAlt="въдица"
                 pText="Риболовни места"
                 linkTo={linkFishSpots}
                 shouldScale={pathName.includes("fish-spots")}
             />
             <RegionChoiceCard
-                imgUrl="https://cdn.discordapp.com/attachments/1156335620919152650/1165310579825721456/spoticon.png?ex=6546630c&is=6533ee0c&hm=789298118befac39d5978b6954eab78850977cd54933dbce1cd2d4dcd3c33ec9&"
+                imgUrl="https://i.ibb.co/hffwSTf/spoticon.png"
                 imgAlt="локация"
                 pText="Предложи ми място"
                 linkTo={linkSuggestedSpots}
