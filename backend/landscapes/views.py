454acb2ccdf0b31ce5551e5f16f5d47d7d799f22
Tamer <tamerchoyy@gmail.com>
2023-11-11 21:23:04 +0200

    update discord api  to ibb api

--- backend/landscapes/views.py
+++ backend/landscapes/views.py
@@ -4,12 +4,14 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 from rest_framework.response import Response
 from django.conf import settings
-from discord_webhook import DiscordWebhook
 from .helpers import get_user_by_id, format_datetime
 from .models import Landscape
 from .serializers import LandscapeListSerializer
+import requests
+import base64
 
-DISCORD_HOOK = settings.CONFIG['DISCORD_WEB_HOOK']
+IMGBB_API_KEY = settings.CONFIG['IMGBB_API_KEY']
+IMGBB_API_URL = settings.CONFIG['IMGBB_API_URL']
 
 
 class CreateLandscape(APIView):
@@ -30,31 +32,35 @@ class CreateLandscape(APIView):
 
         max_ten_mb = 1024 * 1024 * 10
         if image_data.size > max_ten_mb:
-            return Response({"message": "Макс. размер 10мб!"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Макс. размер 10мб!"}, status=status.HTTP_400_BAD_REQUEST)  
 
-        webhook = DiscordWebhook(url=DISCORD_HOOK)
-        webhook.add_file(image_data, str(image_data))
+        if not [x in image_data.content_type for x in ("jpg", "jpeg", "png")]:
+            return Response({"message": "Невалидна снимка."}, status=status.HTTP_400_BAD_REQUEST)     
 
         try:
-            response = webhook.execute()
+            payload = {
+                "key": IMGBB_API_KEY,
+                "image": base64.b64encode(image_data.read())
+            }
+
+            response = requests.post(IMGBB_API_URL, payload)
             data = response.json()
+            print(data)
         except Exception as e:
             print(e)
             return Response({"message": "Грешка при качване."}, status=status.HTTP_400_BAD_REQUEST)
 
-        attachments = data.get('attachments')
-        if not attachments:
+        if response.status_code != 200:
             return Response({"message": "Грешка. Пробвайте пак."}, status=status.HTTP_400_BAD_REQUEST)
         
-        if not [x in attachments[0]['content_type'] for x in ("jpg", "jpeg", "png")]:
-            return Response({"message": "Невалидна снимка."}, status=status.HTTP_400_BAD_REQUEST)
+        image_data = data.get('data')
 
-        img_url = attachments[0]['url']
-        img_width = attachments[0]['width']
-        img_height = attachments[0]['height']
+        img_url = image_data.get('url')
+        img_width = image_data.get('width')
+        img_height = image_data.get('height')
         
         if img_width < 200 or img_height < 200:
-            return Response({"message": "Прекалено малка снимка."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"message": "Мин. размер 200х200"}, status=status.HTTP_400_BAD_REQUEST)
 
 
         landscape = Landscape(
