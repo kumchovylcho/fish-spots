6c14c115eb670c327cd41a1087e437ae8cd12448
Tamer <tamerchoyy@gmail.com>
2023-11-13 21:14:44 +0200

    added new path

--- backend/landscapes/urls.py
+++ backend/landscapes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateLandscape, ListLandscapes
+from .views import CreateLandscape, ListLandscapes, DeleteLandscape
 
 urlpatterns = (
     path('create', CreateLandscape.as_view(), name='create_landscape'),
     path('list', ListLandscapes.as_view(), name='list_landscapes'),
+    path('delete/<int:pk>', DeleteLandscape.as_view(), name='delete_landscape'),
 )
