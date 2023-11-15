2a9868a9bae6eacd1da02f986e97a84df3111588
Tamer <tamerchoyy@gmail.com>
2023-11-15 22:19:14 +0200

    added edit url

--- backend/landscapes/urls.py
+++ backend/landscapes/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import CreateLandscape, ListLandscapes, DeleteLandscape
+from .views import (CreateLandscape,
+                    ListLandscapes, 
+                    DeleteLandscape, 
+                    EditLandscape
+                    )
 
 urlpatterns = (
     path('create', CreateLandscape.as_view(), name='create_landscape'),
     path('list', ListLandscapes.as_view(), name='list_landscapes'),
     path('delete/<int:pk>', DeleteLandscape.as_view(), name='delete_landscape'),
+    path('edit/<int:pk>', EditLandscape.as_view(), name='edit_landscape'),
 )
