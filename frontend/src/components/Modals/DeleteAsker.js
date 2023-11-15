4a8e005cc8e378dad32068a0b665227811793233
Tamer <tamerchoyy@gmail.com>
2023-11-15 22:22:53 +0200

    update

--- frontend/src/components/Modals/DeleteAsker.js
+++ frontend/src/components/Modals/DeleteAsker.js
@@ -18,7 +18,7 @@ export default function DeleteAsker({ deleteId, closeModal }) {
     return (
         <article
             className="fixed top-0 left-0 bottom-0 right-0 bg-black/[.50]"
-            onClick={closeModal}
+            onClick={() => closeModal()}
         >
             <div
                 className="absolute left-2/4 top-2/4 bg-cyan-800 -translate-x-2/4 -translate-y-2/4 max-w-xs break-words p-4 rounded text-white"
