38c4fa908439ed98f2df133e492dfd1e9a40795a
Tamer <tamerchoyy@gmail.com>
2023-11-15 22:24:48 +0200

    centered the text on small devices

--- frontend/src/components/RegionChoice/RegionChoice.js
+++ frontend/src/components/RegionChoice/RegionChoice.js
@@ -15,7 +15,7 @@ export default function RegionChoice() {
 
     return (
         <section className="flex items-center flex-col gap-8 py-16 text-5xl">
-            <h2>Избери регион</h2>
+            <h2 className="max-md:text-center">Избери регион</h2>
             <section className="inline-flex gap-10 text-black text-2xl">
                 <button
                     className={`bg-gradient-to-r from-sky-400 to-blue-500 py-3 px-6 rounded-lg hover:opacity-90 duration-75 ${wantedCity === "varna" ? "outline" : ""}`}
