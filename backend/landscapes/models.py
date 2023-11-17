4860728d60bc9d64241c8378d2dd2f22e3055a89
Tamer <tamerchoyy@gmail.com>
2023-11-17 11:40:54 +0200

    added ordering

--- backend/landscapes/models.py
+++ backend/landscapes/models.py
@@ -11,5 +11,8 @@ class Landscape(models.Model):
     author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ('-created_at', )
+
     def __str__(self):
         return self.title
