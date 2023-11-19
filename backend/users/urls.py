1b9d6141776432756fdde17d958caf658448bd73
Tamer <tamerchoyy@gmail.com>
2023-11-19 21:19:34 +0200

    new path

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (UserExistsView, 
-                    EditUserView
+                    EditUserView,
+                    EditUserPasswordView,
                     )
 
 urlpatterns = (
     path("user-exists/", UserExistsView.as_view()),
     path("edit/<username>", EditUserView.as_view()),
+    path("edit-password/<username>", EditUserPasswordView.as_view()),
 )
