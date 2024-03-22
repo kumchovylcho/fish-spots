a9f062e6990510dda5777951a02f7f4f5f769fb7
Tamer <tamerchoyy@gmail.com>
2024-03-22 22:33:37 +0200

    added hamburger icon

---
+++ frontend/src/components/Navigation/Hamburger.js
@@ -0,0 +1,10 @@
+export default function Hamburger({ isOpen, handleHamburgerMenu }) {
+    return (
+        <button onClick={handleHamburgerMenu} className="w-[60px] h-[45px] relative rotate-0 duration-500 ease-in-out">
+            <span className={`block absolute h-[9px] bg-orange-700 rounded-lg opacity-100 duration-200 ease-in-out rotate-0 ${isOpen ? "top-[18px] left-2/4 w-0" : "top-0 left-0 w-full"}`}></span>
+            <span className={`block absolute left-0 top-[18px] h-[9px] w-full bg-orange-700 rounded-lg opacity-100 duration-200 ease-in-out ${isOpen ? "rotate-45" : "rotate-0"}`}></span>
+            <span className={`block absolute left-0 top-[18px] h-[9px] w-full bg-orange-700 rounded-lg opacity-100 duration-200 ease-in-out ${isOpen ? "-rotate-45" : "rotate-0"}`}></span>
+            <span className={`block absolute h-[9px] bg-orange-700 rounded-lg opacity-100 duration-200 ease-in-out rotate-0 ${isOpen ? "top-[18px] left-2/4 w-0" : "top-[36px] left-0 w-full"}`}></span>
+        </button>
+    );
+}
