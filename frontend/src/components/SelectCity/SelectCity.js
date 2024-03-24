b1b26779e639ef7b8c8b6ec69568a06d1445dcd6
Tamer <tamerchoyy@gmail.com>
2024-03-25 00:58:39 +0200

    removed code repetition

--- frontend/src/components/SelectCity/SelectCity.js
+++ frontend/src/components/SelectCity/SelectCity.js
@@ -1,4 +1,6 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { cityRoutes } from '../../util/routes';
+import { bulgarianToLatinCities } from '../../util/bulgarianToLatin';
 
 
 export default function SelectCity({
@@ -7,25 +9,6 @@ export default function SelectCity({
     city2, 
     city3
 }) {
-    const navigate = useNavigate();
-
-    const bulgarianToEnglish = (city) => {
-        const cities = {
-            "Шабла": "shabla",
-            "Кранево": "kranevo",
-            "Варна": "varna",
-            "Бургас": "burgas",
-            "Черноморец": "chernomorets",
-            "Приморско": "primorsko"
-        }
-
-        return cities[city]
-    }
-
-    const weatherCityHandler = (city) => {
-        const latinName = bulgarianToEnglish(city);
-        navigate(`/city/weather?search=${region}&place=${latinName}`)
-    }
 
     return (
         <div className="max-w-3xl mx-auto p-6 text-2xl bg-white rounded-xl mb-20">
@@ -33,21 +16,15 @@ export default function SelectCity({
                 Избери град
             </p>
             <section className="max-md:flex-col max-md:text-center flex justify-around gap-10 text-white">
-                <p 
-                    onClick={() => {weatherCityHandler(city1)}}
-                    className="bg-cyan-700 hover:bg-cyan-800 py-2 px-6 rounded cursor-pointer">                 
-                    {city1}
-                </p>
-                <p 
-                    onClick={() => {weatherCityHandler(city2)}}
-                    className="bg-cyan-700 hover:bg-cyan-800 py-2 px-6 rounded cursor-pointer">
-                    {city2}
-                </p>
-                <p 
-                    onClick={() => {weatherCityHandler(city3)}}
-                    className="bg-cyan-700 hover:bg-cyan-800 py-2 px-6 rounded cursor-pointer">                
-                    {city3}
-                </p>
+                {[city1, city2, city3].map(city => (
+                    <Link 
+                        key={city}
+                        to={`${cityRoutes.weather}?search=${region}&place=${bulgarianToLatinCities[city]}`}
+                        className="bg-cyan-700 hover:bg-cyan-800 py-2 px-6 rounded cursor-pointer"
+                        >                 
+                        {city}
+                    </Link>
+                ))}
             </section>
         </div>
     );
