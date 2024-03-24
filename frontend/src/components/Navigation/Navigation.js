33824e6b9950069bce85d701707c1ee70fe1d8f1
Tamer <tamerchoyy@gmail.com>
2024-03-24 20:04:27 +0200

    added hamburger menu functionality

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,56 +1,103 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import AuthContext from '../../context/AuthContext';
-import { logoutUser } from '../../services/users';
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Hamburger from './Hamburger';
+
+const navigationLinks = {
+    cssClasses: "hover:text-stone-300 duration-300",
+    links: [
+        {linkTo: "/city", text: "Градове"},
+    ]
+};
 
 const Navigation = () => {
-    const {user, authTokens, setAuthTokens, setUser} = useContext(AuthContext)
-    const navigate = useNavigate();
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const handleHamburgerMenu = () => { // used for when the hamburger menu is clicked
+        const isOpen = !hamburgerOpen;
+
+        if (isOpen) {
+            document.body.classList.add("overflow-hidden");
+        } else if (!isOpen) {
+            document.body.classList.remove("overflow-hidden");
+        }
+
+        setHamburgerOpen(isOpen);
+    }
+
+    const handleCloseHamburgerMenu = () => { // used for closing the menu when the click is outside or when you click a link
+        setHamburgerOpen(false);
+        document.body.classList.remove("overflow-hidden");
+    }
+
+    useEffect(() => {
+        const handleClickOutside = event => {
+            if (!event.target.closest('.click-outside')) {
+                handleCloseHamburgerMenu();
+            }
+        };
+
+        if (hamburgerOpen) {
+            document.addEventListener('click', handleClickOutside);
+        } else {
+            document.removeEventListener('click', handleClickOutside);
+        }
+
+        return () => document.removeEventListener('click', handleClickOutside);
+
+    }, [hamburgerOpen]);
 
     return (
-        <header className="flex justify-center p-6 bg-gradient-to-r from-blue-500 to-cyan-500 text-white font-medium text-2xl">
-            <nav className="flex justify-between w-5/6 max-w-screen-2xl max-md:flex-col max-md:items-center">
-                <h1 className="hover:text-stone-300 duration-300 max-md:mb-10 max-md:text-center">
-                    <Link to="/">Риболовни места</Link>
-                </h1>
+        <header className="flex md:justify-center p-6 bg-gradient-to-r from-blue-500 to-cyan-500 text-white font-medium text-2xl">
+            <nav className="max-w-screen-2xl flex md:justify-between max-md:items-center md:w-5/6 max-md:w-full click-outside">
+                <Hamburger 
+                    isOpen={hamburgerOpen} 
+                    handleHamburgerMenu={handleHamburgerMenu} 
+                />
+                
+                <section className="max-md:w-full max-md:text-center">
+                    <Link 
+                        onClick={handleCloseHamburgerMenu} 
+                        className={navigationLinks.cssClasses} 
+                        to="/"
+                        >
+                        Риболовни места
+                    </Link>
+                </section>
 
-                <ul className="flex gap-12 flex-wrap max-md:flex-col max-md:items-center">
-                    <li className="hover:text-stone-300 duration-300">
-                        <Link to="/city">Градове</Link>
-                    </li>
-                    <li className="hover:text-stone-300 duration-300">
-                        <Link to="/landscapes">Пейзажи</Link>
-                    </li>
+                <ul className="flex gap-12 flex-wrap max-md:hidden">
+                    {navigationLinks.links.map(link => (
+                        <li key={link.text}>
+                            <Link 
+                                className={navigationLinks.cssClasses} 
+                                to={link.linkTo}
+                                >
+                                {link.text}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
 
-                    {user ? (
-                        <div className="max-md:flex-col max-md:items-center flex gap-7">
-                            <p className="hover:text-stone-300 duration-300">
-                                <Link to={`/profile/${user.username}`}>
-                                    <i className="fa-solid fa-user"></i>
-                                </Link>
-                            </p>
-                            <p 
-                                onClick={() => logoutUser(authTokens, setAuthTokens, setUser, navigate, "/login")}
-                                className="text-red-500 hover:text-red-600 duration-300 hover:cursor-pointer">
-                                <i className="fa-solid fa-right-from-bracket"></i>
-                            </p>
-                        </div>
-                    ) : (
-                        <>
-                            <li className="hover:text-stone-300 duration-300">
-                                <Link to="/login">Влез</Link>
-                            </li>
-                            <li className="hover:text-stone-300 duration-300">
-                                <Link to="/register">Регистрация</Link>
-                            </li>
-                        </>
-                    )}
-
+                <ul
+                    className={`z-50 overflow-auto flex flex-col gap-5 fixed top-[80px] left-0 bottom-0 w-60 p-4 text-white font-medium text-2xl bg-slate-900 rounded-r-lg ease-in-out transition-opacity duration-300 ${hamburgerOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+                    >
+                    {navigationLinks.links.map(link => (
+                        <li 
+                            key={link.text} 
+                            className="border-b-2 border-red-900"
+                            onClick={handleCloseHamburgerMenu}
+                            >
+                            <Link 
+                                className={navigationLinks.cssClasses}
+                                to={link.linkTo}
+                                >
+                                {link.text}
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
     );
 }
-
 
 export default Navigation;
