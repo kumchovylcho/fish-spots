ed797b136423c5e3e712518508dddf532dc02b04
Tamer <tamerchoyy@gmail.com>
2024-03-24 19:58:44 +0200

    removed user related stuff. Handled code repetition

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
-import AuthContext from '../../context/AuthContext';
 import setDocTitle from '../../util/setDocTitle';
-import { useContext } from 'react';
+import CityCard from './CityCard';
+
+const cityCards = [
+    {cityName: "ВАРНА", linkTo: "/city?search=varna", cityData: ["14 места", "Автоматизирани подсказки", "Правилна информация"]},
+    {cityName: "БУРГАС", linkTo: "/city?search=burgas", cityData: ["14 места", "Автоматизирани подсказки", "Правилна информация"]}
+];
 
 const Home = () => {
-    const { user } = useContext(AuthContext);
-
     setDocTitle('Home');
 
     return (
@@ -18,88 +20,35 @@ const Home = () => {
                 ></img>
                 <div className="absolute bg-zinc-900 opacity-60 w-full h-full"></div>
                 <article className="flex flex-col justify-center gap-8 absolute text-white">
-                    <h2 className="text-4xl text-center font-medium tracking-widest">
+                    <h2 className="max-md:text-2xl max-md:px-6 text-4xl text-center font-medium tracking-widest">
                         Тук ще откриете вашите места за{' '}
                         <span className="text-sky-400">риболов!</span>
                     </h2>
 
                     <Link
-                        to="/register"
-                        className={`${
-                            user ? 'hidden' : 'block'
-                        } bg-emerald-400 self-center px-10 py-4 text-2xl rounded-full font-medium hover:bg-teal-600 duration-300 hover:scale-105`}
-                    >
-                        Регистрирай се!
+                        to="/city"
+                        className="bg-emerald-400 self-center px-8 py-3 text-2xl rounded-full font-medium hover:bg-teal-600 duration-300 hover:scale-105"
+                        >
+                        Разгледай!
                     </Link>
                 </article>
             </div>
 
             <article className="py-16">
-                <h2 className="text-center text-4xl mb-24 relative">
+                <h2 className="text-center max-md:text-2xl text-4xl max-md:mb-12 mb-24 relative">
                     Информация за над 25 места!
-                    <div className="absolute top-[115%] left-2/4 -translate-x-2/4 -translate-y-1/2 w-60 h-0.5 bg-cyan-600"></div>
+                    <div className="absolute top-[115%] left-2/4 -translate-x-2/4 -translate-y-1/2 max-md:w-40 w-60 h-0.5 bg-cyan-600"></div>
                 </h2>
 
                 <div className="w-full flex flex-wrap justify-center gap-12 w-3/5">
-                    <Link to="/city">
-                        <section className="max-w-lg bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
-                            <h3 className="text-2xl text-center font-bold mb-6">
-                                ВАРНА
-                            </h3>
-                            <div className="flex justify-center gap-x-12">
-                                <section className="text-2xl">
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        14 места
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        Автоматизирани подсказки
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        Правилна информация
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                </section>
-                            </div>
-                        </section>
-                    </Link>
-
-                    <Link to="/city">
-                        <section className="max-w-lg bg-gray-200 p-8 rounded-3xl hover:scale-105 duration-150 cursor-pointer">
-                            <h3 className="text-2xl text-center font-bold mb-6">
-                                БУРГАС
-                            </h3>
-                            <div className="flex justify-center gap-x-12">
-                                <section className="text-2xl">
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        14 места
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        Автоматизирани подсказки
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                    <p className="mb-3 inline-flex justify-between w-full">
-                                        Правилна информация
-                                        <span>
-                                            <i className="fa-solid fa-check text-white bg-green-500 p-1.5 rounded-full w-8 h-8"></i>
-                                        </span>
-                                    </p>
-                                </section>
-                            </div>
-                        </section>
-                    </Link>
+                    {cityCards.map(city => (
+                        <CityCard 
+                            key={city.cityName}
+                            city={city.cityName}
+                            linkTo={city.linkTo}
+                            cityData={city.cityData}
+                        />
+                    ))}
                 </div>
             </article>
         </main>
