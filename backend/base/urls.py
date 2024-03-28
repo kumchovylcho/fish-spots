3f1081bd0c922008d990411021b6fad5ec94f87f
Tamer <tamerchoyy@gmail.com>
2024-03-28 09:12:10 +0200

    changed directory

---
+++ backend/base/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("", views.get_routes),
+
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path("register/", views.RegistrationView.as_view(), name='response_register'),
+    path("logout/", views.LogoutView.as_view(), name='response_logout'),
+]
