9a6ecb449b246fcdf664ef36410aa69450c7b51c
Tamer <tamerchoyy@gmail.com>
2024-03-28 09:15:36 +0200

    added custom cookie authentication backend

---
+++ backend/users/backends.py
@@ -0,0 +1,30 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from rest_framework.exceptions import AuthenticationFailed, PermissionDenied
+from rest_framework.authentication import CSRFCheck
+
+
+def enforce_csrf(request):
+    check = CSRFCheck(request)
+    check.process_request(request)
+    reason = check.process_view(request, None, (), {})
+    if reason:
+        raise PermissionDenied('CSRF Failed: %s' % reason)
+
+
+class CustomAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        header = self.get_header(request)
+
+        if header is None:
+            raw_token = request.COOKIES.get("access_token") or None
+
+        else:
+            raw_token = self.get_raw_token(header)
+
+        if raw_token is None:
+            raise AuthenticationFailed("Token is expired or it doesn't exist.")
+
+        validated_token = self.get_validated_token(raw_token)
+        enforce_csrf(request)
+        return self.get_user(validated_token), validated_token
