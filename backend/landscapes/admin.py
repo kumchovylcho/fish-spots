66919651278f39cb08b0fc901e6a3dbfca264637
Tamer <tamerchoyy@gmail.com>
2024-03-28 09:32:29 +0200

    removed apps

--- backend/landscapes/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Landscape
-
-@admin.register(Landscape)
-class LandscapeAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'created_at', 'author']
