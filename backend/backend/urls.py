3e593ad10f4293cffdaddf63b29200a88e2b85e9
Tamer <tamerchoyy@gmail.com>
2024-03-28 09:10:41 +0200

    removed url for app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,9 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.api.urls')),
+    path('api/', include('base.urls')),
     path('regions/', include('fish_regions.urls')),
     path('places/', include('fish_places.urls')),
-    path('landscapes/', include('landscapes.urls')),
     path('profile/', include('users.urls')),
 ]
