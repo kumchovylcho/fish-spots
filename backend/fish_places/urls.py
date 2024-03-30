7658beb7a63be27e21aaa6a36e49dde7d9f044a6
Tamer <tamerchoyy@gmail.com>
2024-03-31 00:25:37 +0200

    added create path

--- backend/fish_places/urls.py
+++ backend/fish_places/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = (
-    path('', views.PlacesView.as_view(), name='places'),
+    path("", views.PlacesView.as_view(), name="places"),
+    path("create/", views.CreatePlaceView.as_view(), name="create_place"),
 )
