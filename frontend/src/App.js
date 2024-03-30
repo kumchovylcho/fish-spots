fc1ec6ac01601dcaa828c39a1b4ec5500c331c36
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:30:22 +0200

    removed Register path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import Navigation from './components/Navigation/Navigation';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 import PrivateRoute from './util/PrivateRoute';
 import AuthorizedRoute from './util/AuthorizedRoute';
 import City from './components/City/City';
@@ -20,49 +18,39 @@ function App() {
     return (
         <>
             <div className="flex flex-col min-h-screen bg-zinc-300">
-                <AuthProvider>
-                    <Navigation />
+                <Navigation />
 
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/city" element={<City />} />
-                            <Route path={cityRoutes.weather} element={<Weather />} />
-                            <Route path={cityRoutes.fishSpots} element={<FishSpots />} />
-                            <Route path={cityRoutes.suggestedSpots} element={<SuggestedSpots />} />
-                        <Route
-                            path="/login"
-                            element={
-                                <PrivateRoute navigateTo="/">
-                                    <Login />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/register"
-                            element={
-                                <PrivateRoute navigateTo="/">
-                                    <Register />
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route 
-                            path="/profile/:username"
-                            element={
-                                <AuthorizedRoute navigateTo="/login">
-                                    <Profile />
-                                </AuthorizedRoute>
-                            }
-                        />
-                        <Route
-                            path="/landscapes"
-                            element={<Landscapes />}
-                        />
-                        <Route 
-                            path="*"
-                            element={<NotFound />}
-                        />
-                    </Routes>
-                </AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/city" element={<City />} />
+                    <Route path={cityRoutes.weather} element={<Weather />} />
+                    <Route
+                        path={cityRoutes.fishSpots}
+                        element={<FishSpots />}
+                    />
+                    <Route
+                        path={cityRoutes.suggestedSpots}
+                        element={<SuggestedSpots />}
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <PrivateRoute navigateTo="/">
+                                <Login />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/profile/:username"
+                        element={
+                            <AuthorizedRoute navigateTo="/login">
+                                <Profile />
+                            </AuthorizedRoute>
+                        }
+                    />
+                    <Route path="/landscapes" element={<Landscapes />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
 
                 <Footer />
             </div>
