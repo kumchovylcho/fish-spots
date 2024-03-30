bf496f00eb3f9c28ca6260395fb6f881fb6133e0
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:31:03 +0200

    removed registration validator

--- frontend/src/util/registrationValidator.js
+++
@@ -1,8 +0,0 @@
-export function validateUsername(value) {
-    return /^[A-Za-zА-я0-9]+_*([A-Za-zА-я0-9]+)?$/.test(value);
-}
-
-
-export function validateEmail(value) {
-    return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(value);
-}
