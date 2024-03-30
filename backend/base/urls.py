a2dc35b865adbed8c807b565e61faee969d62d08
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:11:09 +0200

    added authorize url

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -7,9 +7,10 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path("", views.get_routes),
 
+    path("authorize/", views.CheckAuthenticationView.as_view(), name="authorize"),
+
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
-    path("register/", views.RegistrationView.as_view(), name='response_register'),
+
     path("logout/", views.LogoutView.as_view(), name='response_logout'),
 ]
