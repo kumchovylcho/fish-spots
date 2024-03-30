cd62d74b36564312cadb56bb22ef212599ad2bd0
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:30:42 +0200

    added auth provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-
+    <AuthProvider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </AuthProvider>
 );
 
 reportWebVitals();
