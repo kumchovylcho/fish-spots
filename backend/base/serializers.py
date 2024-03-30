c6839ae4d803ada368c2ed09306b67076b60a281
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:10:44 +0200

    removed registration

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,8 +1,4 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
-UserModel = get_user_model()
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -13,20 +9,3 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token['username'] = user.username
 
         return token
-
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={
-                                     'input_type': 'password'})
-
-    class Meta:
-        model = UserModel
-        fields = ('username', 'email', 'password')
-
-    def create(self, validated_data):
-        user = UserModel.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
