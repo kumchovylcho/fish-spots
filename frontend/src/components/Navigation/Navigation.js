661edf9d6f6ccc0d69c0c28199120179eb4c8857
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:24:01 +0200

    added logout anchor tag when the admin is logged in.

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,36 +1,39 @@
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import AuthContext from '../../context/AuthContext';
 import Hamburger from './Hamburger';
+import { logoutUser } from '../../services/users';
 
 const navigationLinks = {
-    cssClasses: "hover:text-stone-300 duration-300",
-    links: [
-        {linkTo: "/city", text: "Градове"},
-    ]
+    cssClasses: 'hover:text-stone-300 duration-300',
+    links: [{ linkTo: '/city', text: 'Градове' }],
 };
 
 const Navigation = () => {
     const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const { isLogged, resetUser } = useContext(AuthContext);
 
-    const handleHamburgerMenu = () => { // used for when the hamburger menu is clicked
+    const handleHamburgerMenu = () => {
+        // used for when the hamburger menu is clicked
         const isOpen = !hamburgerOpen;
 
         if (isOpen) {
-            document.body.classList.add("overflow-hidden");
+            document.body.classList.add('overflow-hidden');
         } else if (!isOpen) {
-            document.body.classList.remove("overflow-hidden");
+            document.body.classList.remove('overflow-hidden');
         }
 
         setHamburgerOpen(isOpen);
-    }
+    };
 
-    const handleCloseHamburgerMenu = () => { // used for closing the menu when the click is outside or when you click a link
+    const handleCloseHamburgerMenu = () => {
+        // used for closing the menu when the click is outside or when you click a link
         setHamburgerOpen(false);
-        document.body.classList.remove("overflow-hidden");
-    }
+        document.body.classList.remove('overflow-hidden');
+    };
 
     useEffect(() => {
-        const handleClickOutside = event => {
+        const handleClickOutside = (event) => {
             if (!event.target.closest('.click-outside')) {
                 handleCloseHamburgerMenu();
             }
@@ -43,61 +46,94 @@ const Navigation = () => {
         }
 
         return () => document.removeEventListener('click', handleClickOutside);
-
     }, [hamburgerOpen]);
+
+    const handleLogOut = async () => {
+        try {
+            const response = await logoutUser();
+
+            if (response.status === 200) {
+                resetUser();
+            }
+        } catch (error) {
+            console.error('failed to log out.');
+        }
+    };
 
     return (
         <header className="flex md:justify-center p-6 bg-gradient-to-r from-blue-500 to-cyan-500 text-white font-medium text-2xl">
             <nav className="max-w-screen-2xl flex md:justify-between max-md:items-center md:w-5/6 max-md:w-full click-outside">
-                <Hamburger 
-                    isOpen={hamburgerOpen} 
-                    handleHamburgerMenu={handleHamburgerMenu} 
+                <Hamburger
+                    isOpen={hamburgerOpen}
+                    handleHamburgerMenu={handleHamburgerMenu}
                 />
-                
+
                 <section className="max-md:w-full max-md:text-center">
-                    <Link 
-                        onClick={handleCloseHamburgerMenu} 
-                        className={navigationLinks.cssClasses} 
+                    <Link
+                        onClick={handleCloseHamburgerMenu}
+                        className={navigationLinks.cssClasses}
                         to="/"
-                        >
+                    >
                         Риболовни места
                     </Link>
                 </section>
 
                 <ul className="flex gap-12 flex-wrap max-md:hidden">
-                    {navigationLinks.links.map(link => (
+                    {navigationLinks.links.map((link) => (
                         <li key={link.text}>
-                            <Link 
-                                className={navigationLinks.cssClasses} 
+                            <Link
+                                className={navigationLinks.cssClasses}
                                 to={link.linkTo}
-                                >
+                            >
                                 {link.text}
                             </Link>
                         </li>
                     ))}
+                    {isLogged && (
+                        <li
+                            key="logout"
+                            className={`cursor-pointer ${navigationLinks.cssClasses}`}
+                            onClick={handleLogOut}
+                        >
+                            Logout
+                        </li>
+                    )}
                 </ul>
 
                 <ul
-                    className={`z-50 overflow-auto flex flex-col gap-5 fixed top-[80px] left-0 bottom-0 w-60 p-4 text-white font-medium text-2xl bg-slate-900 rounded-r-lg ease-in-out transition-opacity duration-300 ${hamburgerOpen ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-                    >
-                    {navigationLinks.links.map(link => (
-                        <li 
-                            key={link.text} 
+                    className={`z-50 overflow-auto flex flex-col gap-5 fixed top-[80px] left-0 bottom-0 w-60 p-4 text-white font-medium text-2xl bg-slate-900 rounded-r-lg ease-in-out transition-opacity duration-300 ${
+                        hamburgerOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    }`}
+                >
+                    {navigationLinks.links.map((link) => (
+                        <li
+                            key={link.text}
                             className="border-b-2 border-red-900"
                             onClick={handleCloseHamburgerMenu}
-                            >
-                            <Link 
+                        >
+                            <Link
                                 className={navigationLinks.cssClasses}
                                 to={link.linkTo}
-                                >
+                            >
                                 {link.text}
                             </Link>
                         </li>
                     ))}
+                    {isLogged && (
+                        <li
+                            key="logout-mobile"
+                            className={`cursor-pointer ${navigationLinks.cssClasses}`}
+                            onClick={handleLogOut}
+                        >
+                            Logout
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
     );
-}
+};
 
 export default Navigation;
