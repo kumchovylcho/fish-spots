c42678e9ac97813e5fb329dbe4b7c25315d0c6b4
Tamer <tamerchoyy@gmail.com>
2024-03-31 00:25:01 +0200

    added new fields and image

--- backend/fish_places/models.py
+++ backend/fish_places/models.py
@@ -1,26 +1,31 @@
 from django.db import models
-import json
+from django.contrib.auth import get_user_model
+
+
+UserModel = get_user_model()
 
 
 class Place(models.Model):
 
     place = models.CharField(max_length=50)
-
     bg_place_name = models.CharField(max_length=50)
-
     description = models.TextField()
-
-    image_url = models.TextField()
-
+    image = models.ImageField(
+        max_length=255,
+        upload_to="fishing_spot_images/",
+        null=False,
+        blank=False,
+    )
     longitude = models.CharField(max_length=30)
-
     latitude = models.CharField(max_length=30)
-
     region = models.CharField(max_length=30)
-
     max_wind_speed = models.IntegerField()
-
     bad_wind_directions = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(
+        UserModel, on_delete=models.CASCADE, null=False, blank=False
+    )
 
     def __str__(self):
         return self.bg_place_name
