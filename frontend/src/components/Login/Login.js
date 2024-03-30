f75103cf4c0761852ffb5783409d157f0162920b
Tamer <tamerchoyy@gmail.com>
2024-03-30 20:23:05 +0200

    Updated login functionality and changed css

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,62 +1,144 @@
 import { useState, useContext } from 'react';
-import AuthContext from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../services/users';
 import FormError from '../FormError/FormError';
 import setDocTitle from '../../util/setDocTitle';
 import Spinner from '../Spinner/Spinner';
+import AuthContext from '../../context/AuthContext';
 
 const Login = () => {
+    const { handleSetUser } = useContext(AuthContext);
     const [passwordEye, setPasswordEye] = useState(false);
-    const { setAuthTokens, setUser } = useContext(AuthContext);
-    const [error, setError] = useState(false);
+    const [formError, setFormError] = useState('');
+    const [usernameError, setUsernameError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
-    setDocTitle("Login");
+    setDocTitle('Login');
 
     const handleLoginClick = async (e) => {
+        e.preventDefault();
+
+        if (!username) {
+            setUsernameError('Username field is required.');
+        }
+        if (!password) {
+            setPasswordError('Password field is required.');
+        }
+
+        if (!username || !password) {
+            return;
+        }
+
+        setUsernameError('');
+        setPasswordError('');
+
         setIsLoading(true);
-        await loginUser(e, setAuthTokens, setUser, navigate, "/", setError);
+        try {
+            const response = await loginUser(username, password);
+            const data = await response.json();
+
+            if (response.status === 200) {
+                handleSetUser(data.username);
+                navigate('/');
+            } else if (response.status === 400) {
+                setFormError('Fields must not be empty.');
+            } else if (response.status === 401) {
+                setFormError(data.detail);
+            }
+        } catch (error) {
+            setFormError('Request failed, please try again later.');
+        }
+
         setIsLoading(false);
-    }
+    };
+
+    const handleOnChange = (
+        value,
+        valueSetter,
+        fieldError,
+        fieldErrorSetter
+    ) => {
+        if (value && fieldError) {
+            fieldErrorSetter('');
+        }
+
+        valueSetter(value);
+    };
 
     return (
         <article className="flex justify-center items-center p-16">
-            <form 
-                onSubmit={handleLoginClick} 
+            <form
+                onSubmit={handleLoginClick}
                 className="flex flex-col basis-1/4 gap-8 p-12 rounded-xl text-xl bg-white"
-                >
-                <section className="flex flex-col gap-4">
-                    <label className="font-medium" htmlFor="username">Потребителско име</label>
-                    <input 
-                        className="p-2 border-solid border-2 border-black rounded-xl" 
-                        type="text" 
-                        id="username" 
-                        name="username">
-                    </input>
-                    {error ? <FormError msg={"Невалидно потребителско име или парола."}/> : ""}
+            >
+                <section className="flex flex-col">
+                    <label className="font-medium" htmlFor="username">
+                        Потребителско име
+                    </label>
+                    <input
+                        className={`p-2 border-solid border-2 rounded-xl ${
+                            usernameError ? 'border-red-700' : 'border-black'
+                        }`}
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) =>
+                            handleOnChange(
+                                e.target.value,
+                                setUsername,
+                                usernameError,
+                                setUsernameError
+                            )
+                        }
+                    ></input>
+                    {usernameError && <FormError msg={usernameError} />}
                 </section>
 
-                <section className="flex flex-col gap-4">
-                    <label className="font-medium" htmlFor="password">Парола</label>
+                <section className="flex flex-col">
+                    <label className="font-medium" htmlFor="password">
+                        Парола
+                    </label>
                     <section className="flex relative items-center">
-                        <input 
-                            className="p-2 border-solid border-2 border-black rounded-xl w-full" 
-                            type={passwordEye ? "text": "password"}
+                        <input
+                            className={`p-2 border-solid border-2 rounded-xl w-full ${
+                                passwordError
+                                    ? 'border-red-700'
+                                    : 'border-black'
+                            }`}
+                            type={passwordEye ? 'text' : 'password'}
                             id="password"
-                            name="password">
-                        </input>
+                            name="password"
+                            value={password}
+                            onChange={(e) =>
+                                handleOnChange(
+                                    e.target.value,
+                                    setPassword,
+                                    passwordError,
+                                    setPasswordError
+                                )
+                            }
+                        ></input>
                         <i
                             onClick={() => setPasswordEye(!passwordEye)}
-                            className={`fa-solid ${passwordEye ? "fa-eye" : "fa-eye-slash"} absolute right-6 bottom-[0.8rem] hover:cursor-pointer`}>
-                        </i>
+                            className={`fa-solid ${
+                                passwordEye ? 'fa-eye' : 'fa-eye-slash'
+                            } absolute right-6 bottom-[0.8rem] hover:cursor-pointer`}
+                        ></i>
                     </section>
+                    {passwordError && <FormError msg={passwordError} />}
                 </section>
 
-                <button 
+                {formError && <FormError msg={formError} />}
+
+                <button
                     type="submit"
-                    className="bg-[#10ACDB] text-white rounded-xl p-4 hover:bg-cyan-800">
+                    className="bg-[#10ACDB] text-white rounded-xl p-4 hover:bg-cyan-800"
+                >
                     Влез
                 </button>
 
@@ -64,7 +146,6 @@ const Login = () => {
             </form>
         </article>
     );
-
-}
+};
 
 export default Login;
