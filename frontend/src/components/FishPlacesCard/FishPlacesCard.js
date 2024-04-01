3255b196bd8136dfd1ea4ef0383393d6cf6db3a1
Tamer <tamerchoyy@gmail.com>
2024-04-02 01:29:14 +0300

    changed props.image

--- frontend/src/components/FishPlacesCard/FishPlacesCard.js
+++ frontend/src/components/FishPlacesCard/FishPlacesCard.js
@@ -1,31 +1,26 @@
 export default function FishPlacesCard({ props, modalOpen }) {
-
     return (
         <section className="text-center rounded p-3 bg-cyan-950 shadow-2xl shadow-black">
             <h3 className="font-medium mb-2 text-white">
                 {props.bg_place_name}
             </h3>
             <div>
-                <img 
+                <img
                     className="w-full aspect-[4/3] rounded-t"
-                    src={props.image_url}
+                    src={props.image}
                     alt={props.bg_place_name}
                     loading="lazy"
                 />
             </div>
             <section className="bg-white rounded-b py-2 max-h-[200px] px-1">
-                <p className="mb-3">
-                    {props.description.slice(0, 50)}...
-                </p>
-                <button 
+                <p className="mb-3">{props.description.slice(0, 50)}...</p>
+                <button
                     className="text-center bg-cyan-600 py-2 px-4 rounded-xl font-medium hover:bg-cyan-800"
                     onClick={() => modalOpen(props.id)}
-                    >
+                >
                     Виж повече
                 </button>
             </section>
         </section>
-
-        
     );
 }
