be418377d7ff64b2c3fe420abe590a0aead10b1a
Tamer <tamerchoyy@gmail.com>
2024-04-02 01:31:09 +0300

    now saving the user id on login

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -42,7 +42,7 @@ const Login = () => {
             const data = await response.json();
 
             if (response.status === 200) {
-                handleSetUser(data.username);
+                handleSetUser(data.user, data.id);
                 navigate('/');
             } else if (response.status === 400) {
                 setFormError('Fields must not be empty.');
