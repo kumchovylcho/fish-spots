16cdfa249295a70e4ec2d26ed332cd0ffd2b55b2
Tamer <tamerchoyy@gmail.com>
2024-04-02 01:27:18 +0300

    update fields

--- backend/fish_places/serializers.py
+++ backend/fish_places/serializers.py
@@ -3,7 +3,6 @@ from fish_places.models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
-
     bad_wind_directions = serializers.SerializerMethodField()
 
     class Meta:
@@ -20,14 +19,16 @@ class PlaceSerializer(serializers.ModelSerializer):
             "bad_wind_directions",
             "created_at",
             "last_modified",
+            "id",
         )
 
     def get_bad_wind_directions(self, obj):
         return obj.bad_wind_directions.split(", ")
 
 
-class CreatePlaceSerializer(PlaceSerializer):
-    class Meta(PlaceSerializer.Meta):
+class CreatePlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
         fields = (
             "place",
             "bg_place_name",
@@ -38,4 +39,6 @@ class CreatePlaceSerializer(PlaceSerializer):
             "region",
             "max_wind_speed",
             "bad_wind_directions",
+            "creator",
+            "id",
         )
