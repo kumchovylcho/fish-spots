59e5c1ba155a70db9ad14b246e1692caf14043be
Tamer <tamerchoyy@gmail.com>
2024-04-02 01:32:28 +0300

    added createFishPlace

--- frontend/src/services/fish-spots.js
+++ frontend/src/services/fish-spots.js
@@ -4,3 +4,12 @@ export const getFishPlaces = async (region) => {
     const response = await fetch(`${baseUrl}/places/?region=${region}`);
     return await response.json();
 };
+
+export const createFishPlace = async (formData) => {
+    const options = {
+        method: 'POST',
+        body: formData,
+        credentials: 'include',
+    };
+    return await fetch(`${baseUrl}/places/create/`, options);
+};
