7515b113e1e1e1eab34abd5bdead26170c0973cb
Tamer <tamerchoyy@gmail.com>
2024-04-03 23:14:18 +0300

    added delete path

--- backend/fish_places/urls.py
+++ backend/fish_places/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = (
     path("", views.PlacesView.as_view(), name="places"),
     path("create/", views.CreatePlaceView.as_view(), name="create_place"),
+    path("delete/<int:pk>/", views.DeletePlaceView.as_view(), name="delete_place"),
 )
