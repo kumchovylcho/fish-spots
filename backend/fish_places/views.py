b9f68f2aecd29d284b06fc917a35a6787bd04475
Tamer <tamerchoyy@gmail.com>
2024-04-03 23:17:17 +0300

    added DeletePlaceView

--- backend/fish_places/views.py
+++ backend/fish_places/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import DestroyAPIView
 
 from .models import Place
 from .serializers import PlaceSerializer, CreatePlaceSerializer
@@ -19,7 +20,9 @@ class PlacesView(APIView):
             )
 
         region_places = Place.objects.filter(region=region)
-        serializer = PlaceSerializer(region_places, many=True)
+        serializer = PlaceSerializer(
+            region_places, context={"request": request}, many=True
+        )
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -27,8 +30,14 @@ class PlacesView(APIView):
 class CreatePlaceView(AuthorizedMixin, APIView):
 
     def post(self, request, *args, **kwargs):
-        serializer = CreatePlaceSerializer(data=request.data)
+        serializer = CreatePlaceSerializer(
+            data=request.data, context={"request": request}
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeletePlaceView(AuthorizedMixin, DestroyAPIView):
+    queryset = Place.objects.all()
