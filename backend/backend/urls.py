3fddd9ecbb1d8f6b84fbb5175b116d6b4f600673
Tamer <tamerchoyy@gmail.com>
2024-04-03 23:12:40 +0300

    added static path to debug images

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
-    path('regions/', include('fish_regions.urls')),
-    path('places/', include('fish_places.urls')),
-    path('profile/', include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("base.urls")),
+    path("regions/", include("fish_regions.urls")),
+    path("places/", include("fish_places.urls")),
+    path("profile/", include("users.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
