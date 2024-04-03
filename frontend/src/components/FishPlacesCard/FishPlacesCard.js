bd5b65d916d24fa0d94f986c5cf0d93f53ac8d50
Tamer <tamerchoyy@gmail.com>
2024-04-03 23:21:43 +0300

    added delete button only for authorized people.

--- frontend/src/components/FishPlacesCard/FishPlacesCard.js
+++ frontend/src/components/FishPlacesCard/FishPlacesCard.js
@@ -1,4 +1,9 @@
-export default function FishPlacesCard({ props, modalOpen }) {
+export default function FishPlacesCard({
+    props,
+    modalOpen,
+    openDeleteModal,
+    isLogged,
+}) {
     return (
         <section className="text-center rounded p-3 bg-cyan-950 shadow-2xl shadow-black">
             <h3 className="font-medium mb-2 text-white">
@@ -12,7 +17,11 @@ export default function FishPlacesCard({ props, modalOpen }) {
                     loading="lazy"
                 />
             </div>
-            <section className="bg-white rounded-b py-2 max-h-[200px] px-1">
+            <section
+                className={`bg-white py-2 max-h-[200px] px-1 ${
+                    isLogged ? '' : 'rounded-b'
+                }`}
+            >
                 <p className="mb-3">{props.description.slice(0, 50)}...</p>
                 <button
                     className="text-center bg-cyan-600 py-2 px-4 rounded-xl font-medium hover:bg-cyan-800"
@@ -21,6 +30,16 @@ export default function FishPlacesCard({ props, modalOpen }) {
                     Виж повече
                 </button>
             </section>
+            {isLogged && (
+                <section className="bg-slate-400 py-2 rounded-b">
+                    <button
+                        className="bg-cyan-600 py-2 px-4 rounded-xl font-medium hover:bg-cyan-800"
+                        onClick={openDeleteModal}
+                    >
+                        <i className="fa-solid fa-trash"></i>
+                    </button>
+                </section>
+            )}
         </section>
     );
 }
