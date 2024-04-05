939c53a133b5b07167706e7582eab294c5c915e5
Tamer <tamerchoyy@gmail.com>
2024-04-05 23:04:25 +0300

    refactored by adding abstraction

--- backend/fish_regions/views.py
+++ backend/fish_regions/views.py
@@ -2,8 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import VarnaRegion, BurgasRegion
 from . import helpers
+from . import settings as region_settings
 
 from django.core.cache import cache
 
@@ -14,31 +14,30 @@ class WeatherDataView(APIView):
     def get(self, request, *args, **kwargs):
         region = request.GET.get("region", "").lower()
 
-        regions = {
-            "varna": {"model": VarnaRegion,
-                      "places": ["Shabla", "Kranevo", "Varna"]
-                      },
-            "burgas": {"model": BurgasRegion,
-                       "places": ["Burgas", "Chernomorets", "Primorsko"]
-                       }
-        }
-
-        if not regions.get(region):
-            return Response({"message": "Valid regions are varna/burgas."}, status=status.HTTP_400_BAD_REQUEST)
+        if not region_settings.regions.get(region):
+            return Response(
+                {
+                    "message": f"Valid regions are {list(region_settings.regions.keys())}"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         cached_weather = cache.get(region)
         if cached_weather:
             return Response(cached_weather, status=status.HTTP_200_OK)
 
         regions_data = {}
-        for place in regions[region]["places"]:
-            data_24h = helpers.get_24h_data(regions[region]["model"], place)
+        for place in region_settings.regions[region]["places"]:
+            data_24h = helpers.get_24h_data(
+                region_settings.regions[region]["model"], place
+            )
             four_days_data = helpers.get_four_days_data(
-                regions[region]["model"], place)
+                region_settings.regions[region]["model"], place
+            )
 
             regions_data[place.lower()] = {
                 "today": data_24h,
-                "four_days": four_days_data
+                "four_days": four_days_data,
             }
 
         cache.set(region, regions_data, timeout=1 * 60 * 20)  # 20 minutes
@@ -53,23 +52,28 @@ class WeatherPlaceView(APIView):
         region = request.GET.get("region", "").lower()
         place = place.lower()
 
-        if not region or region not in ["varna", "burgas"]:
-            return Response({"message": "You must pass a valid region. Example -> varna/burgas"}, status=status.HTTP_400_BAD_REQUEST)
+        if not region or region not in region_settings.regions:
+            return Response(
+                {
+                    "message": f"You must pass a valid region. Valid regions are - {list(region_settings.regions.keys())}"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        valid_places = ("shabla", "kranevo", "varna",
-                        "burgas", "chernomorets", "primorsko")
-        if place not in valid_places:
-            return Response({"message": f"You must pass a valid place. Example -> {valid_places}"}, status=status.HTTP_400_BAD_REQUEST)
+        if place not in region_settings.valid_places:
+            return Response(
+                {
+                    "message": f"You must pass a valid place. Valid places are - {region_settings.valid_places}"
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
-        model = VarnaRegion if region == "varna" else BurgasRegion
+        model = region_settings.regions[region]["model"]
 
         place = place.capitalize()
         data_24h = helpers.get_24h_data(model, place)
         four_days_data = helpers.get_four_days_data(model, place)
 
-        place_data = {
-            "today": data_24h,
-            "four_days": four_days_data
-        }
+        place_data = {"today": data_24h, "four_days": four_days_data}
 
         return Response(place_data, status=status.HTTP_200_OK)
