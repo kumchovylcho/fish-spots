e5942f381bbfaa3a3977ea3ac0588ccdf57f374a
Tamer <tamerchoyy@gmail.com>
2024-04-06 20:44:28 +0300

    added more constants and changed file name

---
+++ frontend/src/util/constants.js
@@ -0,0 +1,31 @@
+const baseUrl = 'http://127.0.0.1:8000';
+
+const fishSpotAreasInRegion = {
+    varna: ['shabla', 'kranevo', 'varna'],
+    burgas: ['burgas', 'chernomorets', 'primorsko'],
+};
+
+const bulgarianToLatinCities = {
+    Шабла: 'shabla',
+    Кранево: 'kranevo',
+    Варна: 'varna',
+    Бургас: 'burgas',
+    Черноморец: 'chernomorets',
+    Приморско: 'primorsko',
+};
+
+const latinToBulgarianCities = {
+    shabla: 'Шабла',
+    kranevo: 'Кранево',
+    varna: 'Варна',
+    burgas: 'Бургас',
+    chernomorets: 'Черноморец',
+    primorsko: 'Приморско',
+};
+
+export {
+    baseUrl,
+    fishSpotAreasInRegion,
+    bulgarianToLatinCities,
+    latinToBulgarianCities,
+};
