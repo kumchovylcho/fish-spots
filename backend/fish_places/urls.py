10fa6af0cb0a606ea04a6dcb7de114119202f883
Tamer <tamerchoyy@gmail.com>
2024-04-06 20:51:10 +0300

    added new path

--- backend/fish_places/urls.py
+++ backend/fish_places/urls.py
@@ -5,4 +5,5 @@ urlpatterns = (
     path("", views.PlacesView.as_view(), name="places"),
     path("create/", views.CreatePlaceView.as_view(), name="create_place"),
     path("delete/<int:pk>/", views.DeletePlaceView.as_view(), name="delete_place"),
+    path("suggested-spots/", views.SuggestedSpots.as_view(), name="suggested_spots"),
 )
