e369089131511268e2fb211724c2e74f8cc913dc
Tamer <tamerchoyy@gmail.com>
2024-04-06 20:41:13 +0300

    refactor useEffect

--- frontend/src/components/SuggestedSpots/SuggestedSpots.js
+++ frontend/src/components/SuggestedSpots/SuggestedSpots.js
@@ -4,11 +4,9 @@ import RegionChoice from '../RegionChoice/RegionChoice';
 import ChoiceContainer from '../ChoiceContainer/ChoiceContainer';
 import FilterFishSpots from '../FilterFishSpots/FilterFishSpots';
 import Spinner from '../Spinner/Spinner';
-import { getSuggestedSpots } from './getSuggestedSpots';
 import { useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { getFishPlaces } from '../../services/fish-spots';
-import { getWeather } from '../../services/weather';
+import { getSuggestedSpots } from '../../services/fish-spots';
 import setDocTitle from '../../util/setDocTitle';
 
 export default function SuggestedSpots() {
@@ -16,44 +14,37 @@ export default function SuggestedSpots() {
     const searchParams = new URLSearchParams(city.search);
     const wantedRegion = searchParams.get('search');
 
-    const regionForWeather = wantedRegion === 'varna' ? 'varna' : 'burgas';
-    const regionForFishSpot = wantedRegion === 'varna' ? 'north' : 'south';
-
     const [filteredSpots, setFilteredSpots] = useState([]);
     const [emptyFilter, setEmptyFilter] = useState(false);
-    const [fishSpots, setFishSpots] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [placeDetails, setPlaceDetails] = useState({});
     const [suggestedSpots, setSuggestedSpots] = useState([]);
 
-    setDocTitle("Suggested Spots");
+    setDocTitle('Suggested Spots');
 
     useEffect(() => {
-        const promises = [
-            getWeather(regionForWeather),
-            getFishPlaces(regionForFishSpot),
-        ];
+        setIsLoading(true);
+        getSuggestedSpots(wantedRegion)
+            .then((response) => {
+                if (response.status === 400) {
+                    throw new Error('Региона е невалиден.');
+                }
 
-        Promise.all(promises)
-            .then((results) => {
-                const weatherData = results[0];
-                const fishSpotData = results[1];
-
-                setFishSpots(fishSpotData);
-                setSuggestedSpots(getSuggestedSpots(weatherData, fishSpotData));
-                setIsLoading(false);
+                return response.json();
             })
-
-
-            .catch((err) => {});
+            .then((data) => setSuggestedSpots(data))
+            .catch((error) => {
+                console.error(error.message);
+            })
+            .finally(() => setIsLoading(false));
     }, []);
 
     const filterFishSpots = (searchSpot) => {
         searchSpot = searchSpot.toLowerCase();
         const userFilterSpots = [];
 
-        for (const data of fishSpots) {
+        for (const data of suggestedSpots) {
             if (
                 data.place.includes(searchSpot) ||
                 data.bg_place_name.includes(searchSpot)
@@ -73,7 +64,7 @@ export default function SuggestedSpots() {
     const openModal = (id) => {
         setIsModalOpen(true);
 
-        const place = fishSpots.filter((obj) => obj.id === id)[0];
+        const place = suggestedSpots.filter((obj) => obj.id === id)[0];
 
         setPlaceDetails((oldPlace) => {
             return { ...oldPlace, ...place };
@@ -107,39 +98,43 @@ export default function SuggestedSpots() {
 
             {!isLoading && suggestedSpots.length ? (
                 <>
-                    {suggestedSpots.length && <FilterFishSpots filterFishSpots={filterFishSpots} />}
-                    
-                    {emptyFilter && <p class="text-2xl font-medium text-center mb-4">Няма намерени резултати.</p>}
+                    {suggestedSpots.length && (
+                        <FilterFishSpots filterFishSpots={filterFishSpots} />
+                    )}
 
-                    {!emptyFilter && 
-                        <div 
-                            className="max-w-7xl mx-auto grid grid-cols-4 max-[1000px]:grid-cols-3 max-md:grid-cols-2 max-[460px]:grid-cols-1 gap-12 py-8 px-4 bg-slate-400 rounded-xl mb-16"
-                            >
-                            {(filteredSpots.length ? filteredSpots : fishSpots).map(
-                                (obj) => (
-                                    <FishPlacesCard
-                                        key={obj.id}
-                                        props={obj}
-                                        modalOpen={openModal}
-                                    />
-                                )
-                            )}
+                    {emptyFilter && (
+                        <p className="text-2xl font-medium text-center mb-4">
+                            Няма намерени резултати.
+                        </p>
+                    )}
+
+                    {!emptyFilter && (
+                        <div className="max-w-7xl mx-auto grid grid-cols-4 max-[1000px]:grid-cols-3 max-md:grid-cols-2 max-[460px]:grid-cols-1 gap-12 py-8 px-4 bg-slate-400 rounded-xl mb-16">
+                            {(filteredSpots.length
+                                ? filteredSpots
+                                : suggestedSpots
+                            ).map((obj) => (
+                                <FishPlacesCard
+                                    key={obj.id}
+                                    props={obj}
+                                    modalOpen={openModal}
+                                />
+                            ))}
                         </div>
-                    }
+                    )}
                 </>
             ) : (
                 <h3 className="text-2xl py-12 text-center">
-                    Няма предложени места или информацията е недостатъчна за да се изчисли.
+                    Няма предложени места или информацията е недостатъчна за да
+                    се изчисли.
                 </h3>
             )}
 
-
             <FishPlaceDetails
-                data={placeDetails} 
+                data={placeDetails}
                 isOpen={isModalOpen}
-                closeModal={closeModal} 
+                closeModal={closeModal}
             />
-
         </main>
     );
 }
