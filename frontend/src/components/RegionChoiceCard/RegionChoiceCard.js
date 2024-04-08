54e78ff1d8867926d030ceb7b973668c216a8596
Tamer <tamerchoyy@gmail.com>
2024-04-09 01:37:18 +0300

    removed images for mobile

--- frontend/src/components/RegionChoiceCard/RegionChoiceCard.js
+++ frontend/src/components/RegionChoiceCard/RegionChoiceCard.js
@@ -1,29 +1,43 @@
 import { Link } from 'react-router-dom';
 
-
 export default function RegionChoiceCard({
     imgUrl,
     imgAlt,
     pText,
     linkTo,
-    shouldScale
+    shouldScale,
+    isMobile,
 }) {
-
     return (
-        <Link 
-            to={linkTo}
-            className={`p-2 rounded-xl cursor-pointer hover:scale-105 hover:bg-zinc-300 bg-white duration-150 ${shouldScale && "scale-105 bg-zinc-300"}`}
-            >
-            <section>
-                <div className="mb-3">
-                    <img
-                        src={imgUrl}
-                        className="mx-auto"
-                        alt={imgAlt}
-                    />
-                </div>
-                <p>{pText}</p>
-            </section>
-        </Link>
+        <>
+            {isMobile ? (
+                <Link
+                    to={linkTo}
+                    className={`p-2 rounded-xl cursor-pointer hover:scale-105 hover:bg-zinc-300 bg-cyan-700 hover:bg-cyan-800 duration-150 ${
+                        shouldScale && 'scale-105 bg-zinc-300'
+                    }`}
+                >
+                    {pText}
+                </Link>
+            ) : (
+                <Link
+                    to={linkTo}
+                    className={`p-2 rounded-xl cursor-pointer hover:scale-105 hover:bg-zinc-300 bg-white duration-150 ${
+                        shouldScale && 'scale-105 bg-zinc-300'
+                    }`}
+                >
+                    <section>
+                        <div className="mb-3">
+                            <img
+                                src={imgUrl}
+                                className="mx-auto"
+                                alt={imgAlt}
+                            />
+                        </div>
+                        <p>{pText}</p>
+                    </section>
+                </Link>
+            )}
+        </>
     );
 }
