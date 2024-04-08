1634e54824fef7d87f113ca00091cc702528e4ee
Tamer <tamerchoyy@gmail.com>
2024-04-09 01:28:04 +0300

    added place detail path

--- backend/fish_places/urls.py
+++ backend/fish_places/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = (
@@ -6,4 +7,9 @@ urlpatterns = (
     path("create/", views.CreatePlaceView.as_view(), name="create_place"),
     path("delete/<int:pk>/", views.DeletePlaceView.as_view(), name="delete_place"),
     path("suggested-spots/", views.SuggestedSpots.as_view(), name="suggested_spots"),
+    path(
+        "<str:region>/<str:place_name>/",
+        views.PlaceDetailsView.as_view(),
+        name="place_details",
+    ),
 )
