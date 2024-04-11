eefb7e4d527d577596239eec081d2138f10d6569
Tamer <tamerchoyy@gmail.com>
2024-04-11 16:23:58 +0300

    wrapper everything with cookieContext

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { CookieConsentProvider } from './context/CookieConsentContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AuthProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </AuthProvider>
+    <CookieConsentProvider>
+        <AuthProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </AuthProvider>
+    </CookieConsentProvider>
 );
 
 reportWebVitals();
