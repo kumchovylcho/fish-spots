44e66d5c8ca0948526b54e2c78a62faee55dbb59
Tamer <tamerchoyy@gmail.com>
2024-04-11 16:22:06 +0300

    added privacy-policy

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,36 +1,51 @@
 import { Link } from 'react-router-dom';
 
+const facebookFishGroups = {
+    cssClasses: 'hover:text-cyan-600 duration-150',
+    links: [
+        {
+            linkTo: 'https://www.facebook.com/groups/338746856576368',
+            text: 'Морски риболов',
+        },
+        {
+            linkTo: 'https://www.facebook.com/groups/643789872697112',
+            text: 'Морски риболов - Варна',
+        },
+        {
+            linkTo: 'https://www.facebook.com/groups/288897695142823',
+            text: 'Черноморски риболов',
+        },
+    ],
+};
 
 const Footer = () => {
-
     return (
         <footer className="bg-gray-700 p-5 mt-auto">
-            <div className="max-w-screen-2xl mx-auto flex justify-center text-white">
+            <div className="max-w-screen-2xl mx-auto flex max-md:flex-col max-md:items-center justify-center gap-10 text-white">
                 <section>
-                    <h3 className="flex justify-center gap-x-1 mb-4">
+                    <h3 className="flex justify-center gap-x-1 mb-2">
                         Рибарски групи
-                        <span><i className="text-cyan-600 fa-brands fa-facebook"></i></span>
+                        <span>
+                            <i className="text-cyan-600 fa-brands fa-facebook"></i>
+                        </span>
                     </h3>
                     <ul className="flex flex-col items-center text-cyan-400">
-                        <li className="hover:text-cyan-600 duration-150">
-                            <Link 
-                                to="https://www.facebook.com/groups/338746856576368" 
-                                target='_blank'>
-                                Морски риболов
-                            </Link>
-                        </li>
-                        <li className="hover:text-cyan-600 duration-150">
-                            <Link 
-                                to="https://www.facebook.com/groups/643789872697112" 
-                                target='_blank'>
-                                Морски риболов - Варна
-                            </Link>
-                        </li>
-                        <li className="hover:text-cyan-600 duration-150">
-                            <Link 
-                                to="https://www.facebook.com/groups/288897695142823" 
-                                target='_blank'>
-                                Черноморски риболов
+                        {facebookFishGroups.links.map((group) => (
+                            <li
+                                key={group.text}
+                                className={facebookFishGroups.cssClasses}
+                            >
+                                <Link to={group.linkTo}>{group.text}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+                <section>
+                    <h3 className="text-center mb-2">За Нас</h3>
+                    <ul className="text-cyan-400">
+                        <li className={facebookFishGroups.cssClasses}>
+                            <Link to="/privacy-policy">
+                                Политика за сигурност
                             </Link>
                         </li>
                     </ul>
@@ -38,8 +53,6 @@ const Footer = () => {
             </div>
         </footer>
     );
-
-}
-
+};
 
 export default Footer;
