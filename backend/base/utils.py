0f17ee841639d15eea869656064ce0cfa59f6aa9
Tamer <tamerchoyy@gmail.com>
2024-04-11 16:08:53 +0300

    slight changes to parameters

--- backend/base/utils.py
+++ backend/base/utils.py
@@ -1,17 +1,14 @@
-def set_token_in_cookie(response,
-                        key: str,
-                        value: str,
-                        max_age: int,
-                        secure=True,
-                        httponly=False,
-                        samesite='None'):
+def set_token_in_cookie(
+    response,
+    key: str,
+    value: str,
+    max_age: int,
+    secure=True,
+    httponly=True,
+    samesite="None",
+):
     response.set_cookie(
-        key,
-        value,
-        max_age=max_age,
-        secure=secure,
-        httponly=httponly,
-        samesite=samesite
+        key, value, max_age=max_age, secure=secure, httponly=httponly, samesite=samesite
     )
 
     return response
