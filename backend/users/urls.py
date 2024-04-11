fa20da6e906324139682b01b590f941c56741188
Tamer <tamerchoyy@gmail.com>
2024-04-11 16:11:40 +0300

    added cookie-consent path

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import (UserExistsView, 
-                    EditUserView,
-                    EditUserPasswordView,
-                    )
+from .views import (
+    UserExistsView,
+    EditUserView,
+    EditUserPasswordView,
+    cookie_consent_view,
+)
 
 urlpatterns = (
     path("user-exists/", UserExistsView.as_view()),
     path("edit/<username>", EditUserView.as_view()),
     path("edit-password/<username>", EditUserPasswordView.as_view()),
+    path("cookie-consent/", cookie_consent_view),
 )
