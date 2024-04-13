ae22932b83aad7b25d2fe9a53c8789a96ac590a7
Tamer <tamerchoyy@gmail.com>
2024-04-13 13:18:07 +0300

    added sitemap path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+
+from fish_places.sitemap import PlaceSitemap
+from base.sitemap import StaticViewSitemap
+
+sitemaps = {"articles": PlaceSitemap, "static": StaticViewSitemap}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,6 +15,12 @@ urlpatterns = [
     path("regions/", include("fish_regions.urls")),
     path("places/", include("fish_places.urls")),
     path("profile/", include("users.urls")),
+    path(
+        "sitemap.xml/",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
 
 if settings.DEBUG:
