2ef9b2249aab928a94522470c979c14dea7d4c6d
Tamer <tamerchoyy@gmail.com>
2024-04-14 15:15:46 +0300

    added one more sitemap

--- backend/base/sitemap.py
+++ backend/base/sitemap.py
@@ -1,4 +1,5 @@
 from django.contrib.sitemaps import Sitemap
+from fish_regions.settings import regions as region_settings
 
 
 class StaticViewSitemap(Sitemap):
@@ -8,6 +9,26 @@ class StaticViewSitemap(Sitemap):
 
     def items(self):
         return ["/", "/privacy-policy"]
+
+    def location(self, item):
+        return item
+
+
+class DynamicPagesSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 1
+    protocol = "https"
+
+    def items(self):
+        items = []
+
+        for region, data in region_settings.items():
+            items.append(f"/city/fish-spots?search={region}")
+            items.append(f"/city/suggested-spots?search={region}")
+            for place in data["places"]:
+                items.append(f"/city/weather?search={region}&place={place.lower()}")
+
+        return items
 
     def location(self, item):
         return item
