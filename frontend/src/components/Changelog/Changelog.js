03403dd51ad422072d77a6724f6efe12a23ff989
Tamer <tamerchoyy@gmail.com>
2024-05-03 12:06:37 +0300

    added changelog for future updates

---
+++ frontend/src/components/Changelog/Changelog.js
@@ -0,0 +1,26 @@
+export default function Changelog() {
+    return (
+        <main className="max-w-6xl mx-auto py-10">
+            <article className="p-6 bg-cyan-950 rounded text-white">
+                <h1 className="text-center text-2xl font-medium uppercase mb-10">
+                    Промени по уебсайта
+                </h1>
+                <section className="mb-10">
+                    <h3 className="text-2xl font-medium text-center">
+                        02/05/2024
+                    </h3>
+                    <ul className="flex flex-col gap-2 list-disc text-xl">
+                        <li>
+                            Добавена е страница за чепарета в която можете да се
+                            свържете с майсторите които изработват чепарета.
+                        </li>
+                        <li>
+                            Добавена е страница в която се описват новостите в
+                            сайта.
+                        </li>
+                    </ul>
+                </section>
+            </article>
+        </main>
+    );
+}
