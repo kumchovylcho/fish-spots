f67d7889ab339cebafc4db8ae395977004bcb801
Tamer <tamerchoyy@gmail.com>
2024-05-03 12:06:58 +0300

    main page for chepareta and a form

---
+++ frontend/src/components/Chepareta/Chepareta.js
@@ -0,0 +1,129 @@
+import FishPlacesCard from '../FishPlacesCard/FishPlacesCard';
+import CheparetaForm from './CheparetaForm';
+import Spinner from '../Spinner/Spinner';
+import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import setDocTitle from '../../util/setDocTitle';
+import AuthContext from '../../context/AuthContext';
+import DeleteAsker from '../Modals/DeleteAsker.js';
+import { baseUrl, latinToBgChepareType } from '../../util/constants';
+
+export default function Chepareta() {
+    const { isLogged } = useContext(AuthContext);
+
+    const [allChepareta, setAllChepareta] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [formOpen, setFormOpen] = useState(false);
+
+    setDocTitle('Fish Spots');
+
+    useEffect(() => {
+        fetch(`${baseUrl}/chepareta/`)
+            .then((response) => response.json())
+            .then((data) => {
+                setAllChepareta(data);
+            })
+            .catch((err) => {})
+            .finally(() => setIsLoading((oldLoading) => !oldLoading));
+    }, []);
+
+    const addNewSellerHandler = (newSeller) => {
+        setAllChepareta((oldChepareta) => [newSeller, ...oldChepareta]);
+    };
+
+    const getCardDescription = (sellerObj) => {
+        const uniqueTypes = [];
+        sellerObj.images.forEach((obj) => {
+            const bgName = latinToBgChepareType[obj.chepare_type];
+            if (uniqueTypes.includes(bgName)) {
+                return;
+            }
+
+            uniqueTypes.push(bgName);
+        });
+
+        return uniqueTypes;
+    };
+
+    return (
+        <main>
+            {isLogged && (
+                <>
+                    <section
+                        className={`flex justify-center ${
+                            formOpen ? '' : 'mb-10'
+                        }`}
+                    >
+                        <button
+                            type="button"
+                            className="px-6 py-3.5 text-base font-medium text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            onClick={() => setFormOpen((isOpen) => !isOpen)}
+                        >
+                            {formOpen ? 'Скрий формата' : 'Създай продавач'}
+                        </button>
+                    </section>
+                    <CheparetaForm
+                        showForm={formOpen}
+                        closeForm={() => setFormOpen(false)}
+                        addNewSellerHandler={addNewSellerHandler}
+                    />
+                </>
+            )}
+
+            {isLoading && <Spinner />}
+
+            {!isLoading && (
+                <>
+                    <h2 className="text-center text-3xl font-medium uppercase">
+                        Майстори:
+                    </h2>
+                    <div className="max-w-7xl mx-auto grid grid-cols-4 max-[1000px]:grid-cols-3 max-md:grid-cols-2 max-[460px]:grid-cols-1 gap-12 py-8 px-4 bg-slate-400 rounded-xl mb-16">
+                        {allChepareta.map((seller) => (
+                            <section
+                                key={seller.name}
+                                className="text-center rounded p-3 bg-cyan-950 shadow-2xl shadow-black"
+                            >
+                                <h3 className="font-medium mb-2 text-white">
+                                    {seller.name}
+                                </h3>
+                                <div>
+                                    <img
+                                        className="w-full aspect-[4/3] rounded-t"
+                                        src={seller.images[0].image}
+                                        alt="Чепаре"
+                                        loading="lazy"
+                                    />
+                                </div>
+                                <section
+                                    className={`bg-white py-2 max-h-[200px] px-1 ${
+                                        isLogged ? '' : 'rounded-b'
+                                    }`}
+                                >
+                                    <section className="flex flex-col items-center gap-1">
+                                        <span>Чепарета за:</span>
+                                        <p>
+                                            {getCardDescription(seller).join(
+                                                ', '
+                                            )}
+                                        </p>
+                                    </section>
+                                    <section className="flex justify-center">
+                                        <Link
+                                            className="bg-cyan-600 py-2 px-4 rounded-xl font-medium hover:bg-cyan-800"
+                                            to={`/chepareta/${seller.name
+                                                .replace(' ', '-')
+                                                .toLowerCase()}`}
+                                        >
+                                            Детайли
+                                        </Link>
+                                    </section>
+                                </section>
+                            </section>
+                        ))}
+                    </div>
+                </>
+            )}
+        </main>
+    );
+}
---
+++ frontend/src/components/Chepareta/CheparetaForm.js
@@ -0,0 +1,165 @@
+import { useState } from 'react';
+import { baseUrl } from '../../util/constants';
+import Spinner from '../Spinner/Spinner.js';
+import FormError from '../FormError/FormError.js';
+
+const initialFormData = {
+    name: '',
+    contact: '',
+    imagesData: [],
+};
+
+const chepareTypes = [
+    { text: 'Избери сафридени чепарета', elementName: 'safrid' },
+    { text: 'Избери карагьозени чепарета', elementName: 'karagioz' },
+    { text: 'Избери чернокопени чепарета', elementName: 'chernokop' },
+    { text: 'Избери паламудени чепарета', elementName: 'palamud' },
+];
+
+export default function CheparetaForm({
+    showForm,
+    closeForm,
+    addNewSellerHandler,
+}) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [formData, setFormData] = useState(initialFormData);
+    const [formErrors, setFormErrors] = useState([]);
+
+    const handleFieldChange = (e) => {
+        const { name, value, type } = e.target;
+        if (type !== 'file') {
+            setFormData((oldFormData) => {
+                return {
+                    ...oldFormData,
+                    [name]: value,
+                };
+            });
+        } else if (type === 'file') {
+            setFormData((oldFormData) => {
+                return {
+                    ...oldFormData,
+                    imagesData: [
+                        ...oldFormData.imagesData,
+                        ...Array.from(e.target.files).map((image) => ({
+                            chepare_type: name,
+                            image,
+                        })),
+                    ],
+                };
+            });
+        }
+    };
+
+    const resetForm = () => {
+        setFormData(initialFormData);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const buildFormData = new FormData();
+        for (const key in formData) {
+            if (formData.hasOwnProperty(key)) {
+                if (key === 'imagesData') {
+                    formData[key].forEach((imageData) => {
+                        for (const imageKey in imageData) {
+                            if (imageData.hasOwnProperty(imageKey)) {
+                                buildFormData.append(
+                                    imageKey,
+                                    imageData[imageKey]
+                                );
+                            }
+                        }
+                    });
+                } else {
+                    buildFormData.append(key, formData[key]);
+                }
+            }
+        }
+
+        const options = {
+            method: 'POST',
+            body: buildFormData,
+            credentials: 'include',
+        };
+
+        setIsLoading(true);
+        fetch(`${baseUrl}/chepareta/create/`, options)
+            .then((response) => response.json())
+            .then((data) => {
+                addNewSellerHandler(data);
+                resetForm();
+                closeForm();
+            })
+            .catch((err) => {})
+            .finally(() => setIsLoading(false));
+    };
+
+    return (
+        <form
+            onSubmit={handleFormSubmit}
+            className={`max-w-lg mx-auto mb-10 p-4 rounded-xl flex-col items-center gap-4 bg-gradient-to-b from-slate-700 to-gray-900 ${
+                showForm ? 'flex' : 'hidden'
+            }`}
+            encType="multipart/form-data"
+        >
+            <section className="w-10/12">
+                <span className="text-white dark:font-medium">
+                    Име на продавача:
+                </span>
+                <input
+                    type="text"
+                    className="block w-full rounded p-1.5 text-stone-950 outline-none"
+                    name="name"
+                    maxLength="50"
+                    placeholder="Тамер.."
+                    value={formData.name}
+                    onChange={handleFieldChange}
+                />
+            </section>
+
+            <section className="w-10/12">
+                <span className="text-white dark:font-medium">Контакт:</span>
+                <input
+                    type="text"
+                    className="block w-full rounded p-1.5 text-stone-950 outline-none"
+                    name="contact"
+                    maxLength="50"
+                    placeholder="088.."
+                    value={formData.contact}
+                    onChange={handleFieldChange}
+                />
+            </section>
+
+            {chepareTypes.map((chepare) => (
+                <section key={chepare.text}>
+                    <span className="text-white">{chepare.text}</span>
+                    <input
+                        className="block p-1 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                        type="file"
+                        name={chepare.elementName}
+                        accept=".jpg, .jpeg, .png, .webp"
+                        onChange={handleFieldChange}
+                        multiple
+                    />
+                </section>
+            ))}
+
+            {isLoading && <Spinner />}
+
+            <div className="text-center mt-4">
+                <button className="px-4 py-2 text-black font-medium rounded-lg bg-gradient-to-r from-green-200 via-green-400 to-green-500 hover:from-green-300 hover:via-green-500 hover:to-green-600">
+                    Create!
+                </button>
+            </div>
+
+            {formErrors.length > 0 && (
+                <div className="flex flex-col gap-2 text-lg">
+                    {formErrors.map((error, i) => (
+                        <FormError key={`${error}${i}`} msg={error} />
+                    ))}
+                </div>
+            )}
+        </form>
+    );
+}
