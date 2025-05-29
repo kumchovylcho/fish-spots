8feb320f8d66bb22bb0cf26d59f7d8d028bdcf04
Tamer <tamerchoyy@gmail.com>
2025-05-29 13:58:07 +0300

    added user field and populated the null-ish fields with shell commands

--- backend/catches_history/models.py
+++ backend/catches_history/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 
 class CatchHistory(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True
+    )
     # year-month-day
     date = models.DateField()
     city = models.CharField()
