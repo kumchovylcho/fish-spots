d691201e49ee093c25c53ff0eb09526c723cba05
Tamer <tamerchoyy@gmail.com>
2025-05-29 13:58:21 +0300

    added url

--- backend/catches_history/urls.py
+++ backend/catches_history/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import catch_stats, CreateCatchHistory
+from .views import catch_stats, get_oldest_catch_year, CreateCatchHistory
 
 urlpatterns = (
     path("", catch_stats, name="catch_stats"),
+    path("oldest-catch-year/", get_oldest_catch_year, name="oldest_catch_year"),
     path("create/", CreateCatchHistory.as_view(), name="create_catch_history"),
 )
