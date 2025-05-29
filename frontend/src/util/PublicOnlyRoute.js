a55cf36d1fa01b0edc930073bebb7157d2fea545
Tamer <tamerchoyy@gmail.com>
2025-05-29 14:18:26 +0300

    changed and renamed how private and public guards work. Added comments so its easier to understand

--- frontend/src/util/PublicOnlyRoute.js
+++ frontend/src/util/PublicOnlyRoute.js
@@ -2,15 +2,15 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
-// render content only when logged in
-const PrivateRoute = ({ children, navigateTo = '/login', replace = true }) => {
+// render content only when NOT logged in
+const PublicOnlyRoute = ({ children, navigateTo = '/', replace = true }) => {
     const { isLogged } = useContext(AuthContext);
 
-    if (!isLogged) {
+    if (isLogged) {
         return <Navigate to={navigateTo} replace={replace} />;
     }
 
     return children;
 };
 
-export default PrivateRoute;
+export default PublicOnlyRoute;
