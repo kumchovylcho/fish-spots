256e006facff538cd65f94673a68d0c3a47df6a8
Tamer <tamerchoyy@gmail.com>
2025-05-29 14:02:10 +0300

    overriding save_model() because the password didnt hash when editing/creating a password

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password
 from .models import CustomUser
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        if "password" in form.changed_data:
+            obj.password = make_password(obj.password)
+        super().save_model(request, obj, form, change)
