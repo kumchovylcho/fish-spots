adffe6aeed1c3e14736fbd5ba19c9f672c2f7f09
Tamer <tamerchoyy@gmail.com>
2025-06-04 16:37:48 +0300

    added delete route

--- backend/catches_history/urls.py
+++ backend/catches_history/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import catch_stats, get_oldest_catch_year, CreateCatchHistory
+from .views import (
+    catch_stats,
+    get_oldest_catch_year,
+    delete_catch_history,
+    CreateCatchHistory,
+)
 
 urlpatterns = (
     path("", catch_stats, name="catch_stats"),
     path("oldest-catch-year/", get_oldest_catch_year, name="oldest_catch_year"),
     path("create/", CreateCatchHistory.as_view(), name="create_catch_history"),
+    path("delete/<int:pk>/", delete_catch_history, name="delete_catch_history"),
 )
